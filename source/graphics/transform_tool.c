0b8f2eb38e1c066f5b125699c0a918b2454b3b48
Richard Christie <r.christie@auckland.ac.nz>
2000-06-13 01:35:52 +0000

    Solved compiler warnings.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@480 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/graphics/transform_tool.c
+++ source/graphics/transform_tool.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
 FILE : transform_tool.c
 
-LAST MODIFIED : 12 June 2000
+LAST MODIFIED : 13 June 2000
 
 DESCRIPTION :
 Icon/tool representing the transform function on a graphics window.
@@ -103,14 +103,13 @@ Global functions
 struct Transform_tool *CREATE(Transform_tool)(
 	struct MANAGER(Interactive_tool) *interactive_tool_manager)
 /*******************************************************************************
-LAST MODIFIED : 12 June 2000
+LAST MODIFIED : 13 June 2000
 
 DESCRIPTION :
 Creates a Transform_tool which works like a placeholder for a graphics windows
 own transform mode - so it appears like any other tool in a toolbar.
 ==============================================================================*/
 {
-	char *tool_display_name,*tool_name;
 	struct Transform_tool *transform_tool;
 
 	ENTER(CREATE(Transform_tool));
