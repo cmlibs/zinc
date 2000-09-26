2d1d056c8b290ae8bd4dd8b416600d79417845cd
David Bullivant <bullivan@auckland.ac.nz>
2000-09-26 05:06:24 +0000

    Added ECG
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@892 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/unemap/utilities/make_plaque.c
+++ source/unemap/utilities/make_plaque.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
 FILE : make_plaque.c
 
-LAST MODIFIED : 26 August 2000
+LAST MODIFIED : 26 September 2000
 
 DESCRIPTION :
 Make a unemap configuration file for multiple flexible pcb plaques in a row.
@@ -15,13 +15,16 @@ int main()
 {
 	char file_name[121];
 	FILE *cnfg_file;
-	int electrode_number,i,j,number_of_pcbs,region_number;
+	int ecg_module,electrode_number,i,j,number_of_pcbs,region_number,start1,
+		start2;
 
 	printf("File name? (.cnfg assumed)\n");
 	scanf("%s",file_name);
 	strcat(file_name,".cnfg");
 	if (cnfg_file=fopen(file_name,"wt"))
 	{
+		printf("Number of 64 channel module with ECG attached (0 for none)?\n");
+		scanf("%d",&ecg_module);
 		fprintf(cnfg_file,"mixed : patch\n");
 		region_number=1;
 		electrode_number=0;
@@ -35,7 +38,17 @@ int main()
 				fprintf(cnfg_file,"patch : region %d\n",region_number);
 				for (i=0;i<number_of_pcbs;i++)
 				{
-					for (j=0;j<16;j++)
+					if (ecg_module==(electrode_number/64)+i+1)
+					{
+						start1=2;
+						start2=1;
+					}
+					else
+					{
+						start1=0;
+						start2=0;
+					}
+					for (j=start1;j<16;j++)
 					{
 						fprintf(cnfg_file,"electrode : %d\n",electrode_number+i*64+j+1);
 						fprintf(cnfg_file,"channel : %d\n",electrode_number+i*64+32-2*j);
@@ -49,7 +62,7 @@ int main()
 						fprintf(cnfg_file,"position : x = %d, y = %d\n",j,
 							(number_of_pcbs-i-1)*4+2);
 					}
-					for (j=0;j<16;j++)
+					for (j=start2;j<16;j++)
 					{
 						fprintf(cnfg_file,"electrode : %d\n",electrode_number+i*64+j+33);
 						fprintf(cnfg_file,"channel : %d\n",electrode_number+i*64+31-2*j);
@@ -63,6 +76,28 @@ int main()
 						fprintf(cnfg_file,"position : x = %d, y = %d\n",j,
 							(number_of_pcbs-i-1)*4);
 					}
+				}
+				if ((electrode_number/64<ecg_module)&&
+					(ecg_module<=(electrode_number/64)+number_of_pcbs))
+				{
+					fprintf(cnfg_file,"auxiliary : I\n");
+					fprintf(cnfg_file,"sum : %d-%d\n",(ecg_module-1)*64+30,
+						(ecg_module-1)*64+31);
+					fprintf(cnfg_file,"auxiliary : II\n");
+					fprintf(cnfg_file,"sum : %d-%d\n",(ecg_module-1)*64+32,
+						(ecg_module-1)*64+31);
+					fprintf(cnfg_file,"auxiliary : III\n");
+					fprintf(cnfg_file,"sum : %d-%d\n",(ecg_module-1)*64+32,
+						(ecg_module-1)*64+30);
+					fprintf(cnfg_file,"auxiliary : aVR\n");
+					fprintf(cnfg_file,"sum : %d-0.5*%d-0.5*%d\n",(ecg_module-1)*64+31,
+						(ecg_module-1)*64+30,(ecg_module-1)*64+32);
+					fprintf(cnfg_file,"auxiliary : aVL\n");
+					fprintf(cnfg_file,"sum : %d-0.5*%d-0.5*%d\n",(ecg_module-1)*64+30,
+						(ecg_module-1)*64+31,(ecg_module-1)*64+32);
+					fprintf(cnfg_file,"auxiliary : aVF\n");
+					fprintf(cnfg_file,"sum : %d-0.5*%d-0.5*%d\n",(ecg_module-1)*64+32,
+						(ecg_module-1)*64+30,(ecg_module-1)*64+31);
 				}
 				region_number++;
 				electrode_number += number_of_pcbs*64;
