2d9c38649ca9bc45c5fb901f38f375b8b63a8876
David Bullivant <bullivan@auckland.ac.nz>
2001-03-04 05:13:50 +0000

    For fixing signal file created before the bug to do with indices and auxiliary sums in write_signal_file
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@1142 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/unemap/utilities/fix_ecg.c
@@ -0,0 +1,82 @@
+/*******************************************************************************
+FILE : fix_ecg.c
+
+LAST MODIFIED : 4 March 2001
+
+DESCRIPTION :
+Fixes signal files which were made by write_signal_file prior to fixing the
+bug where signals for devices after auxiliary sums had the wrong indices into
+the signal buffer.  Came to prominence in LA when acquiring with an ecg.
+
+Does not use X.
+==============================================================================*/
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "unemap/rig.h"
+
+/*
+Main program
+------------
+*/
+int main(int argc,char *argv[])
+/*******************************************************************************
+LAST MODIFIED : 4 March 2001
+
+DESCRIPTION :
+==============================================================================*/
+{
+	char file_name[120];
+	FILE *signal_file;
+	int i,j,return_code;
+	struct Device **device;
+	struct Rig *rig;
+	struct Signal *signal;
+
+	return_code=0;
+	rig=(struct Rig *)NULL;
+	/* read the signal file */
+	printf("Signal file name ? ");
+	scanf("%s",file_name);
+	if ((signal_file=fopen(file_name,"rb"))&&read_signal_file(signal_file,&rig))
+	{
+		fclose(signal_file);
+		/* fix the indices */
+		device=rig->devices;
+		i=rig->number_of_devices;
+		j=0;
+		while (i>0)
+		{
+			if ((*device)->channel)
+			{
+				signal=(*device)->signal;
+				while (signal)
+				{
+					signal->index=j;
+					j++;
+					signal=signal->next;
+				}
+			}
+			i--;
+			device++;
+		}
+		/* write out the signal file */
+		printf("New signal file name ? ");
+		scanf("%s",file_name);
+		if ((signal_file=fopen(file_name,"wb"))&&write_signal_file(signal_file,rig))
+		{
+			return_code=1;
+			printf("New signal file created\n");
+		}
+		else
+		{
+			printf("ERROR.  Writing new signal file\n");
+		}
+	}
+	else
+	{
+		printf("ERROR.  Could not read configuration file\n");
+	}
+
+	return (return_code);
+} /* main */
