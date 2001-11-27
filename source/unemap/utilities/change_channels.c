e0593700684514fe3fc8dfc96744b2cfa7db47f5
Richard Christie <r.christie@auckland.ac.nz>
2001-11-27 01:29:58 +0000

    main function now returns 0 on success, non-zero on failure.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@1549 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/unemap/utilities/change_channels.c
+++ source/unemap/utilities/change_channels.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
 FILE : change_channels.h
 
-LAST MODIFIED : 23 September 1997
+LAST MODIFIED : 27 November 2001
 
 DESCRIPTION :
 Reads in a configuration file and a table giving the channel for each electrode.
@@ -18,14 +18,17 @@ Writes out the reordered configuration file.
 Main program
 ------------
 */
-main()
+
+int main()
 {
 	char temp_string[100];
 	FILE *configuration_file,*translation_table_file;
-	int channel_number,i;
+	int channel_number, i, return_code;
 	struct Device **device;
 	struct Rig *rig;
 
+	/* zero is a successful return */
+	return_code = 0;
 	printf("Configuration file ?\n");
 	scanf("%s",temp_string);
 	if (read_configuration_file(temp_string,(void *)&rig))
@@ -61,17 +64,22 @@ main()
 				else
 				{
 					display_message(ERROR_MESSAGE,"Could not open %s",temp_string);
+					return_code = 3;
 				}
 			}
 			else
 			{
 				display_message(ERROR_MESSAGE,"Invalid translation table\n");
+				return_code = 2;
 			}
 			fclose(translation_table_file);
 		}
 		else
 		{
 			display_message(ERROR_MESSAGE,"Could not open %s",temp_string);
+			return_code = 1;
 		}
 	}
+
+	return (return_code);
 }
