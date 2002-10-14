981fae7387efd08307fd6a222088cb9331715288
David Bullivant <bullivan@auckland.ac.nz>
2002-10-14 02:18:33 +0000

    Added external inputs and the option to number new plaques the same way as old
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@2120 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/unemap/utilities/make_plaque.c
+++ source/unemap/utilities/make_plaque.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
 FILE : make_plaque.c
 
-LAST MODIFIED : 3 August 2001
+LAST MODIFIED : 14 October 2002
 
 DESCRIPTION :
 Make a unemap configuration file for multiple flexible pcb plaques in a row.
@@ -15,9 +15,10 @@ int main()
 {
 	char file_name[121],region_name[121];
 	FILE *cnfg_file;
-	int bipolar,channel,ecg_module,electrode_number,end1,end2,end3,i,j,
-		number_of_pcbs,number_in_row,old_4by16,start1,start2,start3,start4,
-		strip_number,write_ecg,x,x_start;
+	int bipolar,channel,ecg_module,electrode_number,end1,end2,end3,end4,
+		external_module,i,j,number_of_pcbs,number_in_row,old_4by16,
+		old_4by16_numbering,start1,start2,start3,start4,strip_number,write_ecg,
+		write_external,x,x_start;
 
 	printf("File name? (.cnfg assumed)\n");
 	scanf("%s%*c",file_name);
@@ -27,6 +28,18 @@ int main()
 		printf("Number of 64 channel module with ECG attached (0 for none)?\n");
 		scanf("%d%*c",&ecg_module);
 		write_ecg=0;
+		do
+		{
+			printf("Number of 64 channel module with external inputs attached "
+				"(0 for none)?\n");
+			scanf("%d%*c",&external_module);
+			if ((external_module==ecg_module)&&(0!=external_module))
+			{
+				printf(
+					"ECG and external inputs can't be attached to the same module\n");
+			}
+		} while ((external_module==ecg_module)&&(0!=external_module));
+		write_external=0;
 		fprintf(cnfg_file,"mixed : patch\n");
 		do
 		{
@@ -37,10 +50,21 @@ int main()
 			if (('o'==file_name[0])||('O'==file_name[0]))
 			{
 				old_4by16=1;
+				old_4by16_numbering=1;
 			}
 			else
 			{
 				old_4by16=0;
+				printf("Have same electrode number position mapping as 4x16 (y/n) ?\n");
+				scanf("%s%*c",file_name);
+				if (('y'==file_name[0])||('Y'==file_name[0]))
+				{
+					old_4by16_numbering=1;
+				}
+				else
+				{
+					old_4by16_numbering=0;
+				}
 			}
 			printf("Bipolar (b) or unipolar (u) ?\n");
 			scanf("%s%*c",file_name);
@@ -90,6 +114,7 @@ int main()
 									start3=1;
 									end3=number_in_row;
 									start4=0;
+									end4=number_in_row;
 #endif /* defined (OLD_CODE) */
 									start1=0;
 									end1=number_in_row;
@@ -98,6 +123,7 @@ int main()
 									start3=0;
 									end3=number_in_row-1;
 									start4=0;
+									end4=number_in_row;
 								}
 								else
 								{
@@ -108,17 +134,48 @@ int main()
 									start3=1;
 									end3=number_in_row;
 									start4=1;
+									end4=number_in_row;
 								}
 							}
 							else
 							{
-								start1=0;
-								end1=number_in_row;
-								start2=0;
-								end2=number_in_row;
-								start3=0;
-								end3=number_in_row;
-								start4=0;
+								if ((external_module==(electrode_number/64)+i+1)&&bipolar)
+								{
+									/*???DB.  Currently only for bipolar */
+									if (old_4by16)
+									{
+										start1=0;
+										end1=number_in_row;
+										start2=0;
+										end2=number_in_row-4;
+										start3=0;
+										end3=number_in_row;
+										start4=0;
+										end4=number_in_row-4;
+									}
+									else
+									{
+										start1=4;
+										end1=number_in_row;
+										start2=0;
+										end2=number_in_row;
+										start3=4;
+										end3=number_in_row;
+										start4=0;
+										end4=number_in_row;
+									}
+								}
+								else
+								{
+									start1=0;
+									end1=number_in_row;
+									start2=0;
+									end2=number_in_row;
+									start3=0;
+									end3=number_in_row;
+									start4=0;
+									end4=number_in_row;
+								}
 							}
 							for (j=start1;j<end1;j++)
 							{
@@ -137,14 +194,29 @@ int main()
 								}
 								else
 								{
-									x=number_in_row-1-j;
-									if (bipolar)
+									if (old_4by16_numbering)
 									{
-										channel=electrode_number+i*64+1+2*j;
+										x=j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+1+2*(start1+end1-1-j);
+										}
+										else
+										{
+											channel=electrode_number+i*64+33+2*(start1+end1-1-j);
+										}
 									}
 									else
 									{
-										channel=electrode_number+i*64+33+2*j;
+										x=number_in_row-1-j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+1+2*j;
+										}
+										else
+										{
+											channel=electrode_number+i*64+33+2*j;
+										}
 									}
 								}
 								fprintf(cnfg_file,"channel : %d\n",channel);
@@ -169,14 +241,29 @@ int main()
 								}
 								else
 								{
-									x=number_in_row-1-j;
-									if (bipolar)
+									if (old_4by16_numbering)
 									{
-										channel=electrode_number+i*64+33+2*j;
+										x=j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+33+2*(start2+end2-1-j);
+										}
+										else
+										{
+											channel=electrode_number+i*64+1+2*(start2+end2-1-j);
+										}
 									}
 									else
 									{
-										channel=electrode_number+i*64+1+2*j;
+										x=number_in_row-1-j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+33+2*j;
+										}
+										else
+										{
+											channel=electrode_number+i*64+1+2*j;
+										}
 									}
 								}
 								fprintf(cnfg_file,"channel : %d\n",channel);
@@ -201,21 +288,36 @@ int main()
 								}
 								else
 								{
-									x=number_in_row-1-j;
-									if (bipolar)
+									if (old_4by16_numbering)
 									{
-										channel=electrode_number+i*64+2+2*j;
+										x=j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+2+2*(start3+end3-1-j);
+										}
+										else
+										{
+											channel=electrode_number+i*64+34+2*(start3+end3-1-j);
+										}
 									}
 									else
 									{
-										channel=electrode_number+i*64+34+2*j;
+										x=number_in_row-1-j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+2+2*j;
+										}
+										else
+										{
+											channel=electrode_number+i*64+34+2*j;
+										}
 									}
 								}
 								fprintf(cnfg_file,"channel : %d\n",channel);
 								fprintf(cnfg_file,"position : x = %d, y = %d\n",x_start+x,
 									(number_of_pcbs-i-1)*4+1);
 							}
-							for (j=start4;j<number_in_row;j++)
+							for (j=start4;j<end4;j++)
 							{
 								fprintf(cnfg_file,"electrode : %d\n",
 									electrode_number+i*64+j+49);
@@ -233,14 +335,29 @@ int main()
 								}
 								else
 								{
-									x=number_in_row-1-j;
-									if (bipolar)
+									if (old_4by16_numbering)
 									{
-										channel=electrode_number+i*64+34+2*j;
+										x=j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+34+2*(start4+end4-1-j);
+										}
+										else
+										{
+											channel=electrode_number+i*64+2+2*(start4+end4-1-j);
+										}
 									}
 									else
 									{
-										channel=electrode_number+i*64+2+2*j;
+										x=number_in_row-1-j;
+										if (bipolar)
+										{
+											channel=electrode_number+i*64+34+2*j;
+										}
+										else
+										{
+											channel=electrode_number+i*64+2+2*j;
+										}
 									}
 								}
 								fprintf(cnfg_file,"channel : %d\n",channel);
@@ -259,6 +376,11 @@ int main()
 						(ecg_module<=(electrode_number/64)+number_of_pcbs)&&bipolar)
 					{
 						write_ecg=1;
+					}
+					if ((electrode_number/64<external_module)&&
+						(external_module<=(electrode_number/64)+number_of_pcbs)&&bipolar)
+					{
+						write_external=1;
 					}
 					electrode_number += number_of_pcbs*64;
 				}
@@ -320,6 +442,26 @@ int main()
 			fprintf(cnfg_file,"auxiliary : aVF\n");
 			fprintf(cnfg_file,"sum : ecg_%d-0.5*ecg_%d-0.5*ecg_%d\n",
 				(ecg_module-1)*64+34,(ecg_module-1)*64+2,(ecg_module-1)*64+33);
+		}
+		if (write_external)
+		{
+			fprintf(cnfg_file,"patch : external\n");
+			fprintf(cnfg_file,"auxiliary : A1\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+1);
+			fprintf(cnfg_file,"auxiliary : A2\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+2);
+			fprintf(cnfg_file,"auxiliary : A3\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+3);
+			fprintf(cnfg_file,"auxiliary : A4\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+4);
+			fprintf(cnfg_file,"auxiliary : A5\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+5);
+			fprintf(cnfg_file,"auxiliary : A6\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+6);
+			fprintf(cnfg_file,"auxiliary : A7\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+7);
+			fprintf(cnfg_file,"auxiliary : A8\n");
+			fprintf(cnfg_file,"channel : %d\n",(external_module-1)*64+8);
 		}
 		fclose(cnfg_file);
 	}
