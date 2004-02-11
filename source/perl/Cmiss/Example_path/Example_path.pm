179e13a9e94e102679920529b05599b2c5338a97
Shane Blackett <s.blackett@auckland.ac.nz>
2004-02-11 22:38:34 +0000

    Added a module to launder the $example variable.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@2976 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/perl/Cmiss/Example_path/Example_path.pm
@@ -0,0 +1,75 @@
+package Cmiss::Example_path;
+
+use 5.006;
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Cmiss::Value ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	$example
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+);
+
+our $VERSION = '0.01';
+
+#Currently implemented by loading cmgui command data and then copying the
+#variable from the default execution package (cmiss).
+#I have put it into this separate module so that in the future we may
+#find the $example directory without the full command_data.
+
+use Cmiss::Cmgui_command_data;
+our $example = $cmiss::example;
+
+1;
+__END__
+
+=head1 NAME
+
+Cmiss::Example_path - Perl extension to export example directory
+
+=head1 SYNOPSIS
+
+  use Cmiss::Example_path qw($example);
+
+=head1 ABSTRACT
+
+  Exports the $example directory from Cmgui.
+
+=head1 DESCRIPTION
+
+  Exports the $example directory from Cmgui.
+
+=head2 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Shane Blackett, <s.blackett@auckland.ac.nz>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2004 by Auckland UniServices
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
+
