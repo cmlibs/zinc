af00c86950743cbd27af500c31d17a04263bb6b2
Shane Blackett <s.blackett@auckland.ac.nz>
2004-04-22 04:39:17 +0000

    Added a line_width to the graphical element settings.  When this is non zero it overrides the default cmgui line width for that settings.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3072 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/finite_element/finite_element_to_streamlines.c
+++ source/finite_element/finite_element_to_streamlines.c
@@ -1598,8 +1598,8 @@ stream vector is tracked, and the travel_scalar is made negative.
 				{
 					gt_data_type=g_NO_DATA;
 				}
-				if (!(polyline=CREATE(GT_polyline)(g_PLAIN,number_of_stream_points,
-					stream_points,/* normals */(Triple *)NULL,
+				if (!(polyline=CREATE(GT_polyline)(g_PLAIN,,/*line_width=default*/0,
+					number_of_stream_points,stream_points,/* normals */(Triple *)NULL,
 					gt_data_type,stream_data)))
 				{
 					display_message(ERROR_MESSAGE,
