4979a726ba0c214eb97b44b4d7a2c94a71c8dbcf
zhang <zhang@auckland.ac.nz>
2004-07-02 02:35:12 +0000

    Added the function image_correlation/weighted average
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3249 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/image_processing/computed_field_image_correlation.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+FILE : computed_field_image_correlation.h
+
+LAST MODIFIED : 23 June 2004
+
+DESCRIPTION :
+Implements image processing operations on computed fields.
+==============================================================================*/
+#if !defined (COMPUTED_FIELD_IMAGE_CORRELATION_H)
+#define COMPUTED_FIELD_IMAGE_CORRELATION_H
+
+int Computed_field_register_types_image_correlation(
+	struct Computed_field_package *computed_field_package,
+	struct Cmiss_region *root_region,
+	struct Graphics_buffer_package *graphics_buffer_package);
+/*******************************************************************************
+LAST MODIFIED : 23 June 2004
+
+DESCRIPTION :
+==============================================================================*/
+
+#endif /* !defined (COMPUTED_FIELD_IMAGE_CORRELATION_H) */
