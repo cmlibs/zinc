7b5c3c9d8c03b3a89f8e07e323f3781bc05d6ea5
David Bullivant <bullivan@auckland.ac.nz>
2004-08-20 03:33:19 +0000

    Changed set so that it unwraps wrapper variables
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3327 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/computed_variable/function_variable_value_specific_implementation.cpp
+++ source/computed_variable/function_variable_value_specific_implementation.cpp
@@ -1,10 +1,12 @@
 //******************************************************************************
 // FILE : function_variable_value_specific_implementation.cpp
 //
-// LAST MODIFIED : 21 July 2004
+// LAST MODIFIED : 18 August 2004
 //
 // DESCRIPTION :
 //==============================================================================
+//
+#include "computed_variable/function_variable_wrapper.hpp"
 
 // global classes
 // ==============
@@ -37,7 +39,16 @@ bool Function_variable_value_specific<Value_type>::set(Value_type& value,
 	result=false;
 	if (set_function)
 	{
-		result=(*set_function)(value,variable);
+		Function_variable_handle wrapped_variable;
+		Function_variable_wrapper_handle temp_variable;
+
+		wrapped_variable=variable;
+		while (temp_variable=boost::dynamic_pointer_cast<Function_variable_wrapper,
+			Function_variable>(wrapped_variable))
+		{
+			wrapped_variable=temp_variable->get_wrapped();
+		}
+		result=(*set_function)(value,wrapped_variable);
 	}
 
 	return (result);
