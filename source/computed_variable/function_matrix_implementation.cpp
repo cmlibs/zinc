b774abd08b8b787b7fd17388ef3086ef5bee67a7
David Bullivant <bullivan@auckland.ac.nz>
2004-08-22 03:42:40 +0000

    Trying to fix problem with template specialization Function_matrix<Scalar> 3
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3335 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/computed_variable/function_matrix_implementation.cpp
+++ source/computed_variable/function_matrix_implementation.cpp
@@ -373,47 +373,6 @@ bool Function_matrix<Value_type>::evaluate_derivative(Scalar&,
 	return (false);
 }
 
-#if !defined (__GNUC__)
-EXPORT template<>
-bool Function_matrix<Scalar>::evaluate_derivative(Scalar& derivative,
-	Function_variable_handle atomic_variable,
-	std::list<Function_variable_handle>& atomic_independent_variables)
-//******************************************************************************
-// LAST MODIFIED : 13 August 2004
-//
-// DESCRIPTION :
-//==============================================================================
-{
-	bool result;
-	boost::intrusive_ptr< Function_variable_matrix_coefficients<Scalar> >
-		atomic_dependent_variable,atomic_independent_variable;
-
-	result=false;
-	if ((atomic_dependent_variable=boost::dynamic_pointer_cast<
-		Function_variable_matrix_coefficients<Scalar>,Function_variable>(
-		atomic_variable))&&
-		equivalent(Function_handle(this),atomic_dependent_variable->function())&&
-		(1==atomic_dependent_variable->number_differentiable()))
-	{
-		result=true;
-		if ((1==atomic_independent_variables.size())&&
-			(atomic_independent_variable=boost::dynamic_pointer_cast<
-			Function_variable_matrix_coefficients<Scalar>,Function_variable>(
-			atomic_independent_variables.front()))&&
-			equivalent(atomic_dependent_variable,atomic_independent_variable))
-		{
-			derivative=1;
-		}
-		else
-		{
-			derivative=0;
-		}
-	}
-
-	return (result);
-}
-#endif // !defined (__GNUC__)
-
 EXPORT template<typename Value_type>
 bool Function_matrix<Value_type>::set_value(
 	Function_variable_handle atomic_variable,
