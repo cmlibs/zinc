aa87fa3b63de57cf13ccf3e4272af5633919d855
zhang <zhang@auckland.ac.nz>
2004-09-01 23:42:59 +0000

    Added the function region_maximum for finding the centre of high bright regions.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3365 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/image_processing/computed_field_region_maximum.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+FILE : computed_field_region_maximum.h
+
+LAST MODIFIED : 24 August 2004
+
+DESCRIPTION :
+Implements image processing operations on computed fields.
+==============================================================================*/
+#if !defined (COMPUTED_FIELD_REGION_MAXIMUM_H)
+#define COMPUTED_FIELD_REGION_MAXIMUM_H
+
+int Computed_field_register_types_region_maximum(
+	struct Computed_field_package *computed_field_package,
+	struct Cmiss_region *root_region,
+	struct Graphics_buffer_package *graphics_buffer_package);
+/*******************************************************************************
+LAST MODIFIED : 4 December 2003
+
+DESCRIPTION :
+==============================================================================*/
+
+#endif /* !defined (COMPUTED_FIELD_REGION_MAXIMUM_H) */
