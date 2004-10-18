a388024ce708f0ba033512737176e99822aca49f
David Bullivant <bullivan@auckland.ac.nz>
2004-10-18 03:34:37 +0000

    Removed unused variable
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3420 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/computed_variable/function_matrix_sum.cpp
+++ source/computed_variable/function_matrix_sum.cpp
@@ -1,7 +1,7 @@
 //******************************************************************************
 // FILE : function_matrix_sum.cpp
 //
-// LAST MODIFIED : 1 October 2004
+// LAST MODIFIED : 18 October 2004
 //
 // DESCRIPTION :
 //==============================================================================
@@ -17,7 +17,7 @@ template<>
 Function_handle Function_variable_matrix_sum<Scalar>::evaluate_derivative(
 	std::list<Function_variable_handle>& independent_variables)
 //******************************************************************************
-// LAST MODIFIED : 1 October 2004
+// LAST MODIFIED : 18 October 2004
 //
 // DESCRIPTION :
 // ???DB.  To be done
@@ -25,11 +25,10 @@ Function_handle Function_variable_matrix_sum<Scalar>::evaluate_derivative(
 {
 	boost::intrusive_ptr< Function_matrix_sum<Scalar> > function_matrix_sum;
 	Function_handle result(0);
-	Function_size_type order;
 
 	if ((function_matrix_sum=boost::dynamic_pointer_cast<
 		Function_matrix_sum<Scalar>,Function>(function()))&&
-		(0<(order=independent_variables.size())))
+		(0<independent_variables.size()))
 	{
 		Function_size_type number_of_columns,number_of_dependent_values,
 			number_of_independent_values,number_of_rows;
