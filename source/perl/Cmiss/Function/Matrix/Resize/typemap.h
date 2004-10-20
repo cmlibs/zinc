370206a63b977bd02eb8149c9b9b1628e4348d3f
David Bullivant <bullivan@auckland.ac.nz>
2004-10-20 02:05:26 +0000

    Added matrix resize function
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3426 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/perl/Cmiss/Function/Matrix/Resize/typemap.h
@@ -0,0 +1,7 @@
+#if ! defined (PERL_CMISS_FUNCTION_MATRIX_RESIZE_TYPEMAP_H)
+#define PERL_CMISS_FUNCTION_MATRIX_RESIZE_TYPEMAP_H
+
+#include "perl/Cmiss/Function/Matrix/typemap.h"
+typedef Cmiss_function_id Cmiss__Function__Matrix__Resize;
+
+#endif /* ! defined (PERL_CMISS_FUNCTION_MATRIX_RESIZE_TYPEMAP_H) */
