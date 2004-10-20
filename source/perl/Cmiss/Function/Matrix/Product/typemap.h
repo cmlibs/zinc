9d17e436a2946df17a262cc8aeedf9d375f153e5
David Bullivant <bullivan@auckland.ac.nz>
2004-10-20 01:54:57 +0000

    Fixed typo
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3423 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/perl/Cmiss/Function/Matrix/Product/typemap.h
+++ source/perl/Cmiss/Function/Matrix/Product/typemap.h
@@ -1,5 +1,5 @@
 #if ! defined (PERL_CMISS_FUNCTION_MATRIX_PRODUCT_TYPEMAP_H)
-#define PERL_CMISS_FUNCTION_MATRIX_SUM_TYPEMAP_H
+#define PERL_CMISS_FUNCTION_MATRIX_PRODUCT_TYPEMAP_H
 
 #include "perl/Cmiss/Function/Matrix/typemap.h"
 typedef Cmiss_function_id Cmiss__Function__Matrix__Product;
