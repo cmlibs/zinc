0d3cfee637f5e9e400a81deea0650a0a4ae46927
David Bullivant <bullivan@auckland.ac.nz>
2005-05-10 22:14:15 +0000

    Added Function_derivatnew_identity
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3895 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/computed_variable/function_derivative.hpp
+++ source/computed_variable/function_derivative.hpp
@@ -1,7 +1,7 @@
 //******************************************************************************
 // FILE : function_derivative.hpp
 //
-// LAST MODIFIED : 13 January 2005
+// LAST MODIFIED : 15 April 2005
 //
 // DESCRIPTION :
 //==============================================================================
@@ -114,7 +114,10 @@ class Function_derivatnew : public Function
 #if defined (EVALUATE_RETURNS_VALUE)
 		virtual Function_handle evaluate(Function_variable_handle atomic_variable);
 #else // defined (EVALUATE_RETURNS_VALUE)
+#if defined (OLD_CODE)
+		//???DB.  Force into sub-class
 		virtual bool evaluate(Function_variable_handle atomic_variable);
+#endif // defined (OLD_CODE)
 #endif // defined (EVALUATE_RETURNS_VALUE)
 		bool evaluate_derivative(Scalar& derivative,
 			Function_variable_handle atomic_variable,
@@ -141,6 +144,34 @@ class Function_derivatnew : public Function
 	protected:
 		Function_variable_handle dependent_variable;
 		std::list<Function_variable_handle> independent_variables;
+};
+
+// class Function_derivatnew_identity
+// ----------------------------------
+
+class Function_derivatnew_identity : public Function_derivatnew
+//******************************************************************************
+// LAST MODIFIED : 15 April 2005
+//
+// DESCRIPTION :
+//==============================================================================
+{
+	public:
+		// for construction exception
+		class Construction_exception {};
+		// constructor
+		Function_derivatnew_identity(
+			const Function_variable_handle& dependent_variable,
+			const std::list<Function_variable_handle>& independent_variables);
+		// destructor
+		~Function_derivatnew_identity();
+	// inherited
+	private:
+#if defined (EVALUATE_RETURNS_VALUE)
+		virtual Function_handle evaluate(Function_variable_handle atomic_variable);
+#else // defined (EVALUATE_RETURNS_VALUE)
+		virtual bool evaluate(Function_variable_handle atomic_variable);
+#endif // defined (EVALUATE_RETURNS_VALUE)
 };
 
 #endif /* !defined (__FUNCTION_DERIVATIVE_HPP__) */
