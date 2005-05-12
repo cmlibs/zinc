3f1183bc464cccaf12d6f1e190c93b615cd2a8f1
zhang <zhang@auckland.ac.nz>
2005-05-12 23:13:23 +0000

    Adding the function kernel_matrix. The function can be used to construct a kernel matrix according to the input parameters: radius,sigma, dimension and filter_name. The number of the components in the kernel_matrix field is just the size of the kernel matrix.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@3914 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/image_processing/computed_field_kernel_matrix.h
@@ -0,0 +1,20 @@
+/*******************************************************************************
+FILE : computed_field_kernel_matrix.h
+
+LAST MODIFIED : 11 May 2005
+
+DESCRIPTION :
+Implements constructing kernel matrix on computed fields.
+==============================================================================*/
+#if !defined (COMPUTED_FIELD_KERNEL_MATRIX_H)
+#define COMPUTED_FIELD_KERNEL_MATRIX_H
+
+int Computed_field_register_types_kernel_matrix(
+	struct Computed_field_package *computed_field_package);
+/*******************************************************************************
+LAST MODIFIED : 11 May 2005
+
+DESCRIPTION :
+==============================================================================*/
+
+#endif /* !defined (COMPUTED_FIELD_KERNEL_MATRIX_H) */
