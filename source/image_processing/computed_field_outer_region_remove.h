c18858cba9429f0ed9ac17fca6c427e9deca43a2
zhang <zhang@auckland.ac.nz>
2005-06-28 04:40:10 +0000

    Adding the new function outer_region_remove, for getting the region of interesting.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4019 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/image_processing/computed_field_outer_region_remove.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+FILE : computed_field_outer_region_remove.h
+
+LAST MODIFIED : 3 March 2004
+
+DESCRIPTION :
+Implements region labeling on computed fields.
+==============================================================================*/
+#if !defined (COMPUTED_FIELD_OUTER_REGION_REMOVE_H)
+#define COMPUTED_FIELD_OUTER_REGION_REMOVE_H
+
+int Computed_field_register_types_outer_region_remove(
+	struct Computed_field_package *computed_field_package,
+	struct Cmiss_region *root_region, struct Graphics_buffer_package *graphics_buffer_package);
+/*******************************************************************************
+LAST MODIFIED : 4 March 2004
+
+DESCRIPTION :
+==============================================================================*/
+
+#endif /* !defined (COMPUTED_FIELD_OUTER_REGION_REMOVE_H) */
