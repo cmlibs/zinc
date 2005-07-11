b7de59c8fd342286daf6df871f3745a44444e56f
Shane Blackett <s.blackett@auckland.ac.nz>
2005-07-11 21:33:44 +0000

    Run it right here so we don't need to copy it.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4043 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/utilities/commands2html/commands2html.sh
+++ source/utilities/commands2html/commands2html.sh
@@ -1,7 +1,7 @@
 #! /bin/tcsh -f
 
 set CMGUI = $1
-set AWK_SCRIPT = ${CMISS_ROOT}/cmgui/utilities/commands2html/commands2html.awk
-set HTML_TEMPLATE = ${CMISS_ROOT}/cmgui/utilities/commands2html/template.html
+set AWK_SCRIPT = ${CMISS_ROOT}/cmgui/source/utilities/commands2html/commands2html.awk
+set HTML_TEMPLATE = ${CMISS_ROOT}/cmgui/source/utilities/commands2html/template.html
 
 ${CMGUI} -command_list | awk -v TEMPLATE=${HTML_TEMPLATE} -f ${AWK_SCRIPT}
