3864caba21e8d885a599108cabc927200bc2ca6b
zhang <zhang@auckland.ac.nz>
2005-08-14 21:28:10 +0000

    Adding the function chan_contour, a image segmentation function based on Chan's algorithm. The algorithm is a kind of curve evolution, which don't need the gradient information of the input image.The model is based on Mumford-Shah segmentation techniques and level set method.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4069 3c1deb5b-d424-0410-962d-aba41a686d42

---
+++ source/image_processing/computed_field_chan_contour.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+FILE : computed_field_chan_contour.h
+
+LAST MODIFIED : 12 August 2005
+
+DESCRIPTION :
+Implements contour detection on computed fields.
+==============================================================================*/
+#if !defined (COMPUTED_FIELD_CHAN_CONTOUR_H)
+#define COMPUTED_FIELD_CHAN_CONTOUR_H
+
+int Computed_field_register_types_chan_contour(
+	struct Computed_field_package *computed_field_package,
+	struct Cmiss_region *root_region, struct Graphics_buffer_package *graphics_buffer_package);
+/*******************************************************************************
+LAST MODIFIED : 12 August 2005
+
+DESCRIPTION :
+==============================================================================*/
+
+#endif /* !defined (COMPUTED_FIELD_CHAN_CONTOUR_H) */
