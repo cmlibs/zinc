ebabee49a52e838fccb465e6270395afa7e7de3a
Shane Blackett <s.blackett@auckland.ac.nz>
2005-08-25 01:52:09 +0000

    Removing xvg as we ready for Open Source release
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4082 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/xvg/include/vca_xvg.h
+++
@@ -1,8 +0,0 @@
-#define uint   unsigned int
-#define uchar  unsigned char
-#define ushort unsigned short
-#define ulong  unsigned long
-#define dev_t  unsigned long
-#ifdef VCAA_CODE
-#include <sys/vca.h>
-#endif
