222beb6da6fc1725b7afe329b183751bd8a64cae
zhang <zhang@auckland.ac.nz>
2005-09-18 22:13:36 +0000

    Fixed for deleting UNUSED VARIABLES set_texture_coordinate_field_data.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4133 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/image_processing/computed_field_adaptive_adjust_contrast.c
+++ source/image_processing/computed_field_adaptive_adjust_contrast.c
@@ -970,8 +970,7 @@ already) and allows its contents to be modified.
 	struct Computed_field_adaptive_adjust_contrast_package
 		*computed_field_adaptive_adjust_contrast_package;
 	struct Option_table *option_table;
-	struct Set_Computed_field_conditional_data set_source_field_data,
-		set_texture_coordinate_field_data;
+	struct Set_Computed_field_conditional_data set_source_field_data;
 
 	ENTER(define_Computed_field_type_adaptive_adjust_contrast);
 	if (state&&(field=(struct Computed_field *)field_void)&&
@@ -991,11 +990,12 @@ already) and allows its contents to be modified.
 			Computed_field_has_numerical_components;
 		set_source_field_data.conditional_function_user_data = (void *)NULL;
 		/* texture_coordinate_field */
-		set_texture_coordinate_field_data.computed_field_manager =
+		/*set_texture_coordinate_field_data.computed_field_manager =
 			computed_field_adaptive_adjust_contrast_package->computed_field_manager;
 		set_texture_coordinate_field_data.conditional_function =
 			Computed_field_has_numerical_components;
 		set_texture_coordinate_field_data.conditional_function_user_data = (void *)NULL;
+		*/
 
 		if (computed_field_adaptive_adjust_contrast_type_string ==
 			Computed_field_get_type_string(field))
