11c637f7c8a41e629d66338654ebf93042c51077
Peter Bier <p.bier@auckland.ac.nz>
2007-03-07 21:24:56 +0000

    updated type string to be in line with rest of filter names
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4874 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/image_processing/computed_field_rescaleIntensityImageFilter.cpp
+++ source/image_processing/computed_field_rescaleIntensityImageFilter.cpp
@@ -62,7 +62,7 @@ using namespace CMISS;
 
 namespace {
 
-char computed_field_rescale_intensity_image_filter_type_string[] = "rescale_intensity_image_filter";
+char computed_field_rescale_intensity_image_filter_type_string[] = "rescale_intensity_filter";
 
 class Computed_field_rescale_intensity_image_filter : public Computed_field_ImageFilter
 {
