58554f83c1d25856be4d34bb3fa0b6a5f3de27dd
Peter Bier <p.bier@auckland.ac.nz>
2007-03-07 21:23:23 +0000

    updated type string to be in line with rest of filter names
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@4872 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/image_processing/computed_field_derivativeImageFilter.cpp
+++ source/image_processing/computed_field_derivativeImageFilter.cpp
@@ -62,7 +62,7 @@ using namespace CMISS;
 
 namespace {
 
-char computed_field_derivative_image_filter_type_string[] = "derivative_image_filter";
+char computed_field_derivative_image_filter_type_string[] = "derivative_filter";
 
 class Computed_field_derivative_image_filter : public Computed_field_ImageFilter
 {
