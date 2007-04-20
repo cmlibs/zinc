2ed65b8115384fd1625f6292491288c5981bd83b
Peter Bier <p.bier@auckland.ac.nz>
2007-04-20 03:27:00 +0000

    corrected error where wrong pixel index was being selected
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@5030 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/image_processing/computed_field_ImageFilter.hpp
+++ source/image_processing/computed_field_ImageFilter.hpp
@@ -827,7 +827,18 @@ Evaluate the templated version of this filter
 				typename ImageType::IndexType index;
 				for (i = 0 ; i < dimension ; i++)
 				{
-					index[i] = (long int)(xi[i] * sizes[i] - 0.5);
+					if (xi[i] < 0.0)
+						{
+							index[i] = 0;
+						}
+					else if (xi[i] >= 1.0)
+						{
+							index[i] = sizes[i] - 1;
+						}
+					else
+						{
+							index[i] = (long int)(xi[i] * (FE_value)sizes[i]);
+						}
 				}
 				assign_field_values( outputImage->GetPixel( index ) );
 				return_code = 1;
