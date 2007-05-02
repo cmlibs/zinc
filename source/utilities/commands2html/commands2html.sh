1daa92769afb01b295e7cde3af25b6875ebcab67
Shane Blackett <s.blackett@auckland.ac.nz>
2007-05-02 23:26:53 +0000

    These files have been superseeded by the commands2html.pl perl version.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@5093 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/utilities/commands2html/commands2html.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/tcsh -f
-
-set CMGUI = $1
-set AWK_SCRIPT = ${CMISS_ROOT}/cmgui/source/utilities/commands2html/commands2html.awk
-set HTML_TEMPLATE = ${CMISS_ROOT}/cmgui/source/utilities/commands2html/template.html
-
-${CMGUI} -command_list | awk -v TEMPLATE=${HTML_TEMPLATE} -f ${AWK_SCRIPT}
