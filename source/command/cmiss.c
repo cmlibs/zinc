01618d8b6fca49386b99e1e6c0c7daf1414da92a
Shane Blackett <s.blackett@auckland.ac.nz>
2008-03-18 23:04:14 +0000

    Copy image properties from the Cmgui_image to the Texture so that they are available to the texture api and in "gfx list texture".
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@5692 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/command/cmiss.c
+++ source/command/cmiss.c
@@ -7394,6 +7394,8 @@ Modifies the properties of a texture.
 							{
 								if (cmgui_image = Cmgui_image_read(cmgui_image_information))
 								{
+									char *property, *value;
+
 									return_code = Texture_set_image(texture, cmgui_image,
 										image_data.image_file_name, file_number_pattern,
 										file_number_series_data.start,
@@ -7401,6 +7403,17 @@ Modifies the properties of a texture.
 										file_number_series_data.increment,
 										image_data.crop_left_margin, image_data.crop_bottom_margin,
 										image_data.crop_width, image_data.crop_height);
+									Cmgui_image_get_property(cmgui_image,"exif:*");
+									Cmgui_image_reset_property_iterator(cmgui_image);
+									while ((property = Cmgui_image_get_next_property(
+										cmgui_image)) && 
+										(value = Cmgui_image_get_property(cmgui_image,
+										property)))
+									{
+										Texture_set_property(texture, property, value);
+										DEALLOCATE(property);
+										DEALLOCATE(value);
+									}
 									DESTROY(Cmgui_image)(&cmgui_image);
 								}
 								else
