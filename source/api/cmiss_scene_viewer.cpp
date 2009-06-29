17342c5f6b0efcaa7c6c4663565bd57118f2ae7d
Alan Wu <alan.wu@auckland.ac.nz>
2009-06-29 22:37:49 +0000

    Added create_Cmiss_scene_viewer_wx to the external API. Tracker item 1831, https://tracker.physiomeproject.org/show_bug.cgi?id=1831
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@6444 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/api/cmiss_scene_viewer.cpp
+++ source/api/cmiss_scene_viewer.cpp
@@ -46,6 +46,9 @@ scenes.
 #if defined (GTK_USER_INTERFACE)
 #include <gtk/gtk.h>
 #endif /* defined (GTK_USER_INTERFACE) */
+#if defined (WX_USER_INTERFACE)
+#include <wx/wx.h>
+#endif /* defined (GTK_USER_INTERFACE) */
 extern "C" {
 #include "api/cmiss_scene_viewer.h"
 #include "api/cmiss_scene_viewer_private.h"
@@ -61,6 +64,80 @@ extern "C" {
 Global functions
 ----------------
 */
+
+#if defined (WX_USER_INTERFACE)
+Cmiss_scene_viewer_id create_Cmiss_scene_viewer_wx(
+	struct Cmiss_scene_viewer_package *cmiss_scene_viewer_package,
+	void *parent_void,
+	enum Cmiss_scene_viewer_buffering_mode buffer_mode,
+	enum Cmiss_scene_viewer_stereo_mode stereo_mode,
+	int minimum_colour_buffer_depth, int minimum_depth_buffer_depth,
+	int minimum_accumulation_buffer_depth)
+{
+	enum Graphics_buffer_buffering_mode graphics_buffer_buffering_mode;
+	enum Graphics_buffer_stereo_mode graphics_buffer_stereo_mode;
+	struct Graphics_buffer *graphics_buffer;
+	struct Cmiss_scene_viewer *scene_viewer;
+	wxPanel *parent;
+
+	ENTER(create_Cmiss_scene_viewer_wx);
+	parent = (wxPanel *)parent_void;
+	if (cmiss_scene_viewer_package)
+	{
+		if (CMISS_SCENE_VIEWER_BUFFERING_ANY_MODE==buffer_mode)
+		{
+			graphics_buffer_buffering_mode = GRAPHICS_BUFFER_ANY_BUFFERING_MODE;
+		}
+		else if (CMISS_SCENE_VIEWER_BUFFERING_SINGLE==buffer_mode)
+		{
+			graphics_buffer_buffering_mode = GRAPHICS_BUFFER_SINGLE_BUFFERING;
+		}
+		else if (CMISS_SCENE_VIEWER_BUFFERING_RENDER_OFFSCREEN_AND_COPY==buffer_mode)
+		{
+			graphics_buffer_buffering_mode = GRAPHICS_BUFFER_RENDER_OFFSCREEN_AND_COPY;
+		}
+		else if (CMISS_SCENE_VIEWER_BUFFERING_RENDER_OFFSCREEN_AND_BLEND==buffer_mode)
+		{
+			graphics_buffer_buffering_mode = GRAPHICS_BUFFER_RENDER_OFFSCREEN_AND_BLEND;
+		}
+		else
+		{
+			graphics_buffer_buffering_mode = GRAPHICS_BUFFER_DOUBLE_BUFFERING;
+		}
+		if (CMISS_SCENE_VIEWER_STEREO_ANY_MODE==stereo_mode)
+		{
+			graphics_buffer_stereo_mode = GRAPHICS_BUFFER_ANY_STEREO_MODE;
+		}
+		else if (CMISS_SCENE_VIEWER_STEREO_STEREO==stereo_mode)
+		{
+			graphics_buffer_stereo_mode = GRAPHICS_BUFFER_STEREO;
+		}
+		else
+		{
+			graphics_buffer_stereo_mode = GRAPHICS_BUFFER_MONO;
+		}
+		graphics_buffer = create_Graphics_buffer_wx(
+			Cmiss_scene_viewer_package_get_graphics_buffer_package(cmiss_scene_viewer_package),
+			parent,
+			graphics_buffer_buffering_mode, graphics_buffer_stereo_mode,
+			minimum_colour_buffer_depth, minimum_depth_buffer_depth,
+			minimum_accumulation_buffer_depth, NULL);
+		scene_viewer = create_Scene_viewer_from_package(graphics_buffer,
+			cmiss_scene_viewer_package,
+			Cmiss_scene_viewer_package_get_default_scene(cmiss_scene_viewer_package));
+	}
+	else
+	{
+		display_message(ERROR_MESSAGE,"create_Cmiss_scene_viewer_wx.  "
+			"The Cmiss_scene_viewer data must be initialised before any scene "
+			"viewers can be created.");
+		scene_viewer=(struct Cmiss_scene_viewer *)NULL;
+	}
+	LEAVE;
+
+	return (scene_viewer);
+} /* create_Cmiss_scene_viewer_wx */
+#endif /* defined (WX_USER_INTERFACE) */
 
 #if defined (GTK_USER_INTERFACE)
 Cmiss_scene_viewer_id create_Cmiss_scene_viewer_gtk(
@@ -89,10 +166,7 @@ chosen.
 	struct Cmiss_scene_viewer *scene_viewer;
 
 	ENTER(create_Cmiss_scene_viewer_gtk);
-	/* Not implemented yet */
-	USE_PARAMETER(minimum_colour_buffer_depth);
-	USE_PARAMETER(minimum_accumulation_buffer_depth);
-	USE_PARAMETER(minimum_depth_buffer_depth);
+
 	if (cmiss_scene_viewer_package)
 	{
 		if (CMISS_SCENE_VIEWER_BUFFERING_ANY_MODE==buffer_mode)
--- source/api/cmiss_scene_viewer.h
+++ source/api/cmiss_scene_viewer.h
@@ -257,6 +257,37 @@ Global functions
 ----------------
 */
 
+#if defined (WX_USER_INTERFACE)
+/***************************************************************************//**
+ * Creates a Cmiss_scene_viewer by creating a wxCanvas inside the specified
+ * wxPanel container.If <minimum_colour_buffer_depth>, 
+ * <minimum_depth_buffer_depth> or <minimum_accumulation_buffer_depth> are not 
+ * zero then they are used to filter out the possible visuals selected for 
+ * graphics_buffers.  If they are zero then the accumulation_buffer_depth 
+ * are not tested and the maximum colour buffer depth is chosen.
+ * 
+ * @param cmiss_scene_viewer_package  Package containing information required
+ * @param parent_void  void pointer to a wxPanel in wxWidgets. 
+ * @param buffer_mode  see enum Cmiss_scene_viewer_buffering_mode.
+ * @param stereo_mode  see enum Cmiss_scene_viewer_stereo_mode
+ * @param minimum_colour_buffer_depth  minimum colour buffer depth to be set on 
+ *    scene viewer.
+ * @param minimum_depth_buffer_depth  minimum_depth_buffer_depth to be set on 
+ *    scene viewer.
+ * @param minimum_accumulation_buffer_depth  minimum_accumulation_buffer_depth 
+ *    to be set on scene viewer.
+ * @return  Cmiss_scene_viewer_id if successfully create the scene viewer,
+ *    otherwise NULL.
+ */
+Cmiss_scene_viewer_id create_Cmiss_scene_viewer_wx(
+	struct Cmiss_scene_viewer_package *cmiss_scene_viewer_package,
+	void *parent_void,
+	enum Cmiss_scene_viewer_buffering_mode buffer_mode,
+	enum Cmiss_scene_viewer_stereo_mode stereo_mode,
+	int minimum_colour_buffer_depth, int minimum_depth_buffer_depth,
+	int minimum_accumulation_buffer_depth);
+#endif /* defined (WX_USER_INTERFACE) */
+
 #if defined (GTK_USER_INTERFACE)
 Cmiss_scene_viewer_id create_Cmiss_scene_viewer_gtk(
 	struct Cmiss_scene_viewer_package *cmiss_scene_viewer_package,
