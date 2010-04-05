2a5ab30241fa9a4dc6ed7e712e0fd95fc6c7a303
Shane Blackett <s.blackett@auckland.ac.nz>
2010-04-05 22:53:57 +0000

    Fix leak of imagemagick blob when writing to a memory block. https://tracker.physiomeproject.org/show_bug.cgi?id=2378 Merged from cmgui6735_pluspatch revision 6798. Fix up so that it compiles with MSVC.
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@6978 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/general/image_utilities.c
+++ source/general/image_utilities.c
@@ -7503,15 +7503,15 @@ that the images be adjoined in the single file.
 				}
 				if (cmgui_image_information->write_to_memory_block)
 				{
+					/* Need to use the correct pointer type for the API */
+					size_t magick_memory_block_length = 0;
+
 					/* If we have one already free it */
 					if (cmgui_image_information->memory_block &&
 						cmgui_image_information->memory_block_is_imagemagick_blob)
 					{
 						RelinquishMagickMemory(cmgui_image_information->memory_block);
 					}
-
-					/* Need to use the correct pointer type for the API */
-					size_t magick_memory_block_length = 0;
 
 					SetImageInfo(magick_image_info, MagickFalse, &magick_exception);
 
