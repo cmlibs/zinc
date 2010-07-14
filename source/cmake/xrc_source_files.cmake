502a4a7de288819be1d0b4e4fcea29ef8a5e1964
Hugh Sorby <h.sorby@auckland.ac.nz>
2010-07-14 03:48:54 +0000

    Making the inclusion of cad_tool.xrc conditional on wether OpenCASCADE is being built.
    https://tracker.physiomeproject.org/show_bug.cgi?id=2648
    
    
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/cmgui/trunk@7960 3c1deb5b-d424-0410-962d-aba41a686d42

--- source/cmake/xrc_source_files.cmake
+++ source/cmake/xrc_source_files.cmake
@@ -6,5 +6,7 @@
 	source/element/element_tool.xrc source/graphics/graphics_window.xrc
 	source/graphics/region_tree_viewer_wx.xrc source/graphics/spectrum_editor_wx.xrc
 	source/graphics/transform_tool.xrc source/material/material_editor_wx.xrc
-	source/node/node_tool.xrc source/node/node_viewer_wx.xrc
-	source/cad/cad_tool.xrc )
+	source/node/node_tool.xrc source/node/node_viewer_wx.xrc )
+IF( USE_OPENCASCADE )
+	SET( XRC_SRCS ${XRC_SRCS} source/cad/cad_tool.xrc )
+ENDIF( USE_OPENCASCADE )
