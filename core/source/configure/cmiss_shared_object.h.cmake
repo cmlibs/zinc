c638f8f0c24e1b7ace38a04ecccc85c3f46aa050
Hugh Sorby <h.sorby@auckland.ac.nz>
2012-10-14 23:31:49 +0000

    Changes to match the specification given in tracker item 3242.  https://tracker.physiomeproject.org/show_bug.cgi?id=3242
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/zinc/library/trunk@9432 3c1deb5b-d424-0410-962d-aba41a686d42

--- core/source/configure/cmiss_shared_object.h.cmake
+++ core/source/configure/cmiss_shared_object.h.cmake
@@ -2,27 +2,30 @@
 #ifndef CMISS_SHARED_OBJECT_H
 #define CMISS_SHARED_OBJECT_H
 
+// Handle the exporting of symbols from a dynamic shared object
+// across different oses.
+
 #if defined _WIN32 || defined __CYGWIN__
-  #define ZINC_DLL_IMPORT __declspec(dllimport)
-  #define ZINC_DLL_EXPORT __declspec(dllexport)
+  #define ZINC_DSO_IMPORT __declspec(dllimport)
+  #define ZINC_DSO_EXPORT __declspec(dllexport)
 #else
   #if __GNUC__ >= 4
-    #define ZINC_DLL_IMPORT __attribute__ ((visibility ("default")))
-    #define ZINC_DLL_EXPORT __attribute__ ((visibility ("default")))
+    #define ZINC_DSO_IMPORT __attribute__ ((visibility ("default")))
+    #define ZINC_DSO_EXPORT __attribute__ ((visibility ("default")))
   #else
-    #define ZINC_DLL_IMPORT
-    #define ZINC_DLL_EXPORT
+    #define ZINC_DSO_IMPORT
+    #define ZINC_DSO_EXPORT
   #endif
 #endif
 
 // Now we use the generic helper definitions above to define ZINC_API.
-// ZINC_API is used for the public API symbols. It either DLL imports or DLL exports (or does nothing for static build)
+// ZINC_API is used for the public API symbols. It either DSO imports or DSO exports (or does nothing for static build)
 
 #ifdef ZINC_SHARED_OBJECT // defined if Zinc is compiled as a shared object
-  #ifdef ZINC_EXPORTS // defined if we are building the Zinc DLL (instead of using it)
-    #define ZINC_API ZINC_DLL_EXPORT
+  #ifdef ZINC_EXPORTS // defined if we are building the Zinc DSO (instead of using it)
+    #define ZINC_API ZINC_DSO_EXPORT
   #else
-    #define ZINC_API ZINC_DLL_IMPORT
+    #define ZINC_API ZINC_DSO_IMPORT
   #endif // ZINC_EXPORTS
 #else // ZINC_SHARED_OBJECT is not defined: this means Zinc is a static lib.
   #define ZINC_API
