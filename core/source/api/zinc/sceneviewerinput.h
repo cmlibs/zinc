b713c6d891582d451fe1f94df39913635124ae9c
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-04-05 02:47:36 +0000

    Adding documentation to scene viewer input API functions.  https://tracker.physiomeproject.org/show_bug.cgi?id=3454
    git-svn-id: https://svn.physiomeproject.org/svn/cmiss/zinc/library/trunk@9612 3c1deb5b-d424-0410-962d-aba41a686d42

--- core/source/api/zinc/sceneviewerinput.h
+++ core/source/api/zinc/sceneviewerinput.h
@@ -47,25 +47,91 @@ typedef int Cmiss_scene_viewer_input_modifier;
 extern "C" {
 #endif
 
-//-- ZINC_API int Cmiss_scene_viewer_default_input_callback(Cmiss_scene_viewer_id scene_viewer,
-//-- 	Cmiss_scene_viewer_input_id input);
-
+/**
+ * Create a scene viewer input object for manually setting mouse or other input
+ * event data.
+ *
+ * @param scene_viewer  Handle to Cmiss_scene_viewer object.
+ * @return  Handle to Cmiss_scene_viewer_input on success, or 0 on failure.
+ */
 ZINC_API Cmiss_scene_viewer_input_id Cmiss_scene_viewer_get_input(Cmiss_scene_viewer_id scene_viewer);
 
-ZINC_API int Cmiss_scene_viewer_input_destroy(Cmiss_scene_viewer_input_id *address_input);
-
+/**
+ * Returns a new reference to the Cmiss_scene_viewer_input with reference count incremented.
+ * Caller is responsible for destroying the new reference.
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @return  Accessed handle to Cmiss_scene_viewer_input.
+ */
 ZINC_API Cmiss_scene_viewer_input_id Cmiss_scene_viewer_input_access(Cmiss_scene_viewer_input_id input);
 
+/**
+ * Destroys this reference to the Cmiss_scene_viewer_input (and sets it to 0).
+ * Internally this just decrements the reference count.
+ *
+ * @param address_input  Address of handle to Cmiss_scene_viewer_input object.
+ * @return  CMISS_OK if successfully deaccess Cmiss_scene_viewer_input, any other value on
+ * failure.
+ */
+ZINC_API int Cmiss_scene_viewer_input_destroy(Cmiss_scene_viewer_input_id *address_input);
+
+/**
+ * Set the position of the input
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @param x The x-coordinate of the input position.
+ * @param y The y-coordinate of the input position.
+ * @return  CMISS_OK if successful, any other value on failure.
+ */
 ZINC_API int Cmiss_scene_viewer_input_set_position(Cmiss_scene_viewer_input_id input, int x, int y);
 
+/**
+ * Set modifier for the input.
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @param modifier  Enumerator for the modifier to set.
+ * @return  Handle to Cmiss_scene_viewer_input on success, or 0 on failure.
+ */
 ZINC_API int Cmiss_scene_viewer_input_set_modifier(Cmiss_scene_viewer_input_id input, Cmiss_scene_viewer_input_modifier modifier);
 
+/**
+ * Get the modifier set for the given input.
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @return  Enumerator for the input modifier.
+ */
 ZINC_API Cmiss_scene_viewer_input_modifier Cmiss_scene_viewer_input_get_modifier(Cmiss_scene_viewer_input_id input);
 
+/**
+ * Set the button number for the input.
+ *   1 ==> left mouse button.
+ *   2 ==> middle mouse button.
+ *   3 ==> right mouse button.
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @param number  The numeric value of the button.
+ * @return  CMISS_OK if successful, any other value on failure.
+ */
 ZINC_API int Cmiss_scene_viewer_input_set_button_number(Cmiss_scene_viewer_input_id input, int number);
 
+/**
+ * Create a scene viewer input object for manually setting mouse or other input
+ * event data.
+ *
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @param type  Enumerator for the input event type.
+ * @return  CMISS_OK if successful, any other value on failure.
+ */
 ZINC_API int Cmiss_scene_viewer_input_set_type(Cmiss_scene_viewer_input_id input, Cmiss_scene_viewer_input_event_type type);
 
+/**
+ * Converts mouse button-press and motion events set in the input into viewport zoom
+ * and translate transformations.
+ *
+ * @param scene_viewer  Handle to Cmiss_scene_viewer object.
+ * @param input  Handle to Cmiss_scene_viewer_input object.
+ * @return  CMISS_OK if successful, any other value on failure.
+ */
 ZINC_API int Cmiss_scene_viewer_input_viewport_transform(
 	Cmiss_scene_viewer_id scene_viewer, Cmiss_scene_viewer_input_id input);
 
