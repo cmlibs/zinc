84f281bcea08bddbb74fa378fd4bb72bd95641f2
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-04-10 21:28:23 +0000

    Adding tests for Cmiss_graphic_{set,get}_face and Cmiss_graphic_{set,get}_exterior.  https://tracker.physiomeproject.org/show_bug.cgi?id=3531

--- graphic/graphic.cpp
+++ graphic/graphic.cpp
@@ -39,3 +39,62 @@ TEST(Cmiss_graphic_api, set_use_element_type)
 	Cmiss_region_destroy(&root_region);
 	Cmiss_context_destroy(&context);
 }
+
+TEST(Cmiss_graphic_api, exterior)
+{
+	Cmiss_context_id context = Cmiss_context_create("test");
+	Cmiss_region_id root_region = Cmiss_context_get_default_region(context);
+	Cmiss_field_module_id fm = Cmiss_region_get_field_module(root_region);
+	Cmiss_graphics_module_id gm = Cmiss_context_get_default_graphics_module(context);
+	EXPECT_EQ(CMISS_OK, Cmiss_graphics_module_enable_renditions(gm, root_region));
+	Cmiss_rendition_id ren = Cmiss_graphics_module_get_rendition(gm, root_region);
+
+	EXPECT_NE(static_cast<Cmiss_field_module *>(0), fm);
+	EXPECT_NE(static_cast<Cmiss_graphics_module *>(0), gm);
+	EXPECT_NE(static_cast<Cmiss_rendition *>(0), ren);
+
+	Cmiss_graphic_id is = Cmiss_rendition_create_graphic(ren, CMISS_GRAPHIC_ISO_SURFACES);
+
+	EXPECT_NE(static_cast<Cmiss_graphic *>(0), is);
+
+	int result = Cmiss_graphic_set_exterior(is, 1);
+	EXPECT_EQ(CMISS_OK, result);
+	EXPECT_EQ(1, Cmiss_graphic_get_exterior(is));
+
+	Cmiss_graphic_destroy(&is);
+	Cmiss_rendition_destroy(&ren);
+	Cmiss_graphics_module_destroy(&gm);
+	Cmiss_field_module_destroy(&fm);
+	Cmiss_region_destroy(&root_region);
+	Cmiss_context_destroy(&context);
+}
+
+TEST(Cmiss_graphic_api, face)
+{
+	Cmiss_context_id context = Cmiss_context_create("test");
+	Cmiss_region_id root_region = Cmiss_context_get_default_region(context);
+	Cmiss_field_module_id fm = Cmiss_region_get_field_module(root_region);
+	Cmiss_graphics_module_id gm = Cmiss_context_get_default_graphics_module(context);
+	EXPECT_EQ(CMISS_OK, Cmiss_graphics_module_enable_renditions(gm, root_region));
+	Cmiss_rendition_id ren = Cmiss_graphics_module_get_rendition(gm, root_region);
+
+	EXPECT_NE(static_cast<Cmiss_field_module *>(0), fm);
+	EXPECT_NE(static_cast<Cmiss_graphics_module *>(0), gm);
+	EXPECT_NE(static_cast<Cmiss_rendition *>(0), ren);
+
+	Cmiss_graphic_id is = Cmiss_rendition_create_graphic(ren, CMISS_GRAPHIC_ISO_SURFACES);
+
+	EXPECT_NE(static_cast<Cmiss_graphic *>(0), is);
+
+	int result = Cmiss_graphic_set_face(is, CMISS_GRAPHIC_FACE_XI2_0);
+	EXPECT_EQ(CMISS_OK, result);
+	EXPECT_EQ(CMISS_GRAPHIC_FACE_XI2_0, Cmiss_graphic_get_face(is));
+
+	Cmiss_graphic_destroy(&is);
+	Cmiss_rendition_destroy(&ren);
+	Cmiss_graphics_module_destroy(&gm);
+	Cmiss_field_module_destroy(&fm);
+	Cmiss_region_destroy(&root_region);
+	Cmiss_context_destroy(&context);
+}
+
