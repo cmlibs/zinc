c024beef334a3f930d39b41b3fbfd4b0ff2e3e29
Richard Christie <r.christie@auckland.ac.nz>
2013-04-30 08:30:33 +0000

    Add unit tests for iso surface graphic API get iso values functions. Issue 3361.

--- graphic/iso_surface.cpp
+++ graphic/iso_surface.cpp
@@ -62,6 +62,8 @@ TEST(Cmiss_graphic_iso_surface, iso_scalar_field)
 	Cmiss_graphic_iso_surface_id is = Cmiss_graphic_cast_iso_surface(gr);
 	EXPECT_NE(static_cast<Cmiss_graphic_iso_surface *>(0), is);
 
+	EXPECT_EQ(static_cast<Cmiss_field *>(0), Cmiss_graphic_iso_surface_get_iso_scalar_field(is));
+
 	double values[] = {1.0};
 	Cmiss_field_id c = Cmiss_field_module_create_constant(zinc.fm, 1, values);
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_scalar_field(is, c));
@@ -79,7 +81,7 @@ TEST(Cmiss_graphic_iso_surface, iso_scalar_field)
 	Cmiss_graphic_destroy(&gr);
 }
 
-TEST(Cmiss_graphic_iso_surface, set_iso_values)
+TEST(Cmiss_graphic_iso_surface, iso_values)
 {
 	ZincTestSetup zinc;
 
@@ -88,15 +90,24 @@ TEST(Cmiss_graphic_iso_surface, set_iso_values)
 	Cmiss_graphic_iso_surface_id is = Cmiss_graphic_cast_iso_surface(gr);
 	EXPECT_NE(static_cast<Cmiss_graphic_iso_surface *>(0), is);
 
-	int num = 3;
-	double values[] = {1.0, 1.2, 3.4};
+	double outputValues[4];
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_values(is, 4, outputValues));
+
+	const int num = 3;
+	const double values[] = {1.0, 1.2, 3.4};
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_values(is, num, values));
+	EXPECT_EQ(num, Cmiss_graphic_iso_surface_get_iso_values(is, 4, outputValues));
+	EXPECT_EQ(values[0], outputValues[0]);
+	EXPECT_EQ(values[1], outputValues[1]);
+	EXPECT_EQ(values[2], outputValues[2]);
+	// can ask for just number:
+	EXPECT_EQ(num, Cmiss_graphic_iso_surface_get_iso_values(is, 0, 0));
 
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_destroy(&is));
 	Cmiss_graphic_destroy(&gr);
 }
 
-TEST(Cmiss_graphic_iso_surface, set_iso_values_null)
+TEST(Cmiss_graphic_iso_surface, iso_values_null)
 {
 	ZincTestSetup zinc;
 
@@ -107,15 +118,20 @@ TEST(Cmiss_graphic_iso_surface, set_iso_values_null)
 
 	int num = 3;
 	double values[] = {1.0, 1.2, 3.4};
-	EXPECT_NE(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_values(0, num, values));
-	EXPECT_NE(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_values(is, 5, 0));
-	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_values(is, -1, 0));
+	EXPECT_EQ(CMISS_ERROR_ARGUMENT, Cmiss_graphic_iso_surface_set_iso_values(0, num, values));
+	EXPECT_EQ(CMISS_ERROR_ARGUMENT, Cmiss_graphic_iso_surface_set_iso_values(is, 5, 0));
+	EXPECT_EQ(CMISS_ERROR_ARGUMENT, Cmiss_graphic_iso_surface_set_iso_values(is, -1, 0));
+
+	double outputValues[4];
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_values(0, 4, outputValues));
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_values(is, 4, 0));
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_values(is, -1, 0));
 
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_destroy(&is));
 	Cmiss_graphic_destroy(&gr);
 }
 
-TEST(Cmiss_graphic_iso_surface, set_iso_range)
+TEST(Cmiss_graphic_iso_surface, iso_range)
 {
 	ZincTestSetup zinc;
 
@@ -124,11 +140,43 @@ TEST(Cmiss_graphic_iso_surface, set_iso_range)
 	Cmiss_graphic_iso_surface_id is = Cmiss_graphic_cast_iso_surface(gr);
 	EXPECT_NE(static_cast<Cmiss_graphic_iso_surface *>(0), is);
 
-	int num = 6;
+	double outputFirst, outputLast;
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_range(is, &outputFirst, &outputLast));
+
+	const int num = 6;
 	double first = 0.1, last = 0.55;
-	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, num, first, last));
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, 1, 0.3, 0.3));
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, 1, 0.7, 0.7));
+	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, num, first, last));
+	EXPECT_EQ(num, Cmiss_graphic_iso_surface_get_iso_range(is, &outputFirst, &outputLast));
+	EXPECT_EQ(first, outputFirst);
+	EXPECT_EQ(last, outputLast);
+
+	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_destroy(&is));
+	Cmiss_graphic_destroy(&gr);
+}
+
+TEST(Cmiss_graphic_iso_surface, iso_range_null)
+{
+	ZincTestSetup zinc;
+
+	Cmiss_graphic_id gr = Cmiss_rendition_create_graphic(zinc.ren, CMISS_GRAPHIC_ISO_SURFACES);
+
+	Cmiss_graphic_iso_surface_id is = Cmiss_graphic_cast_iso_surface(gr);
+	EXPECT_NE(static_cast<Cmiss_graphic_iso_surface *>(0), is);
+
+	const int num = 6;
+	double first = 0.1, last = 0.55;
+	EXPECT_EQ(CMISS_ERROR_ARGUMENT, Cmiss_graphic_iso_surface_set_iso_range(0, num, first, last));
+	EXPECT_EQ(CMISS_ERROR_ARGUMENT, Cmiss_graphic_iso_surface_set_iso_range(is, -1, first, last));
+	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, 0, first, last));
+	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_set_iso_range(is, num, first, last));
+
+	double outputFirst, outputLast;
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_range(0, &outputFirst, &outputLast));
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_range(is, 0, &outputLast));
+	EXPECT_EQ(0, Cmiss_graphic_iso_surface_get_iso_range(is, &outputFirst, 0));
+	EXPECT_EQ(num, Cmiss_graphic_iso_surface_get_iso_range(is, &outputFirst, &outputLast));
 
 	EXPECT_EQ(CMISS_OK, Cmiss_graphic_iso_surface_destroy(&is));
 	Cmiss_graphic_destroy(&gr);
