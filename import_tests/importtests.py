13b922f76c8627d7b3a368d7808dfbe8bfb3d12b
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-05-14 23:58:25 +0000

    Renaming the PyZinc unit test python modules to be conformant with PEP8.  Also updating the PyZinc module name from Zn to zinc.  https://tracker.physiomeproject.org/show_bug.cgi?id=3439

---
+++ import_tests/importtests.py
@@ -0,0 +1,83 @@
+import os, sys, unittest
+
+class ImportTestCase(unittest.TestCase):
+
+    def testImportContext(self):
+        from zinc import context
+
+    def testImportDifferentialOperator(self):
+        from zinc import differentialoperator
+    
+    def testImportElement(self):
+        from zinc import element
+    
+    def testImportField(self):
+        from zinc import field
+    
+    def testImportFieldCache(self):
+        from zinc import fieldcache
+    
+    def testImportFieldModule(self):
+        from zinc import fieldmodule
+    
+    def testImportGraphic(self):
+        from zinc import graphic
+    
+    def testImportGraphicsFilter(self):
+        from zinc import graphicsfilter
+    
+    def testImportGraphicsMaterial(self):
+        from zinc import graphicsmaterial
+    
+    def testImportGraphicsModule(self):
+        from zinc import graphicsmodule
+    
+    def testImportNode(self):
+        from zinc import node
+    
+    def testImportOptimisation(self):
+        from zinc import optimisation
+    
+    def testImportRegion(self):
+        from zinc import region
+
+    def testImportRendition(self):
+        from zinc import rendition
+    
+    def testImportScene(self):
+        from zinc import scene
+    
+    def testImportSceneViewer(self):
+        from zinc import sceneviewer
+    
+    def testImportSelection(self):
+        from zinc import selection
+    
+    def testImportSpectrum(self):
+        from zinc import spectrum
+    
+    def testImportStream(self):
+        from zinc import stream
+    
+    def testImportTessellation(self):
+        from zinc import tessellation
+    
+    def testImportTimeKeeper(self):
+        from zinc import timekeeper
+    
+    def testImportTimeNotifier(self):
+        from zinc import timenotifier
+    
+    def testImportTimeSequence(self):
+        from zinc import timesequence
+    
+
+
+def suite():
+    #import ImportTestCase
+    tests = unittest.TestSuite()
+    tests.addTests(unittest.TestLoader().loadTestsFromTestCase(ImportTestCase))
+    return tests
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(suite())
--- runalltests.py
+++ runalltests.py
@@ -4,8 +4,8 @@ import unittest
 if __name__ == '__main__':
     tests = unittest.TestSuite()
 
-    from import_tests import ImportTests
-    tests.addTests(ImportTests.suite())
+    from import_tests import importtests
+    tests.addTests(importtests.suite())
 
     unittest.TextTestRunner().run(tests)
 
