663e74575866ab8ed84f31f3c9ef735c38a75caa
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-05-22 01:54:39 +0000

    Adding to region PyZinc unit test.  https://tracker.physiomeproject.org/show_bug.cgi?id=3568

--- python/region_tests/regiontests.py
+++ python/region_tests/regiontests.py
@@ -5,14 +5,24 @@ Created on May 22, 2013
 '''
 import unittest
 
-from zinc.region import Region
+from zinc.context import Context
+#from zinc.region import Region
 
 class RegionTestCase(unittest.TestCase):
 
-
+    def setUp(self):
+        self.c = Context("region")
+        
+    def tearDown(self):
+        del self.c
+        
     def testRegion(self):
-        r = Region()
-        self.assertTrue(r.isValid())
+        rr = self.c.getDefaultRegion()
+        self.assertTrue(rr.isValid())
+        sr = rr.createSubregion("bob/rick/mark")
+        self.assertTrue(sr.isValid())
+        tr = rr.findSubregionAtPath("/bob/rick/mark")
+        self.assertTrue(tr.isValid())
 
 
 def suite():
