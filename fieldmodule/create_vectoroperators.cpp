1199b507614dc297e43d9e64e82b87bcc05fbced
Richard Christie <r.christie@auckland.ac.nz>
2013-05-24 04:01:01 +0000

    Add unit tests for cross product field in 2 and 3 dimensions. Issue 3569.

--- fieldmodule/create_vectoroperators.cpp
+++ fieldmodule/create_vectoroperators.cpp
@@ -16,7 +16,134 @@
 #include "zinc/fieldtypesconstant.hpp"
 #include "zinc/fieldtypesvectoroperators.hpp"
 
-TEST(Cmiss_field_sum_components, create)
+TEST(Cmiss_field_cross_product, create_evaluate_2d)
+{
+	ZincTestSetup zinc;
+
+	const double values1[] = { 1.0, 1.0 };
+	Cmiss_field_id f1 = Cmiss_field_module_create_constant(zinc.fm, 2, values1);
+	EXPECT_NE((Cmiss_field_id)0, f1);
+
+	Cmiss_field_id f2 = Cmiss_field_module_create_cross_product(zinc.fm, 1, &f1);
+	EXPECT_NE((Cmiss_field_id)0, f2);
+
+	Cmiss_field_cache_id cache = Cmiss_field_module_create_cache(zinc.fm);
+	double values[2];
+	EXPECT_EQ(CMISS_OK, Cmiss_field_evaluate_real(f2, cache, 2, values));
+	ASSERT_DOUBLE_EQ(-1.0, values[0]);
+	ASSERT_DOUBLE_EQ(1.0, values[1]);
+	Cmiss_field_cache_destroy(&cache);
+
+	// test invalid arguments
+	const double one = 1.0;
+	Cmiss_field_id f4 = Cmiss_field_module_create_constant(zinc.fm, 1, &one);
+	EXPECT_NE((Cmiss_field_id)0, f4);
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product(0, 1, &f1));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product(zinc.fm, 0, &f1));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product(zinc.fm, 1, 0));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product(zinc.fm, 1, &f4));
+
+	Cmiss_field_destroy(&f1);
+	Cmiss_field_destroy(&f2);
+	Cmiss_field_destroy(&f4);
+}
+
+TEST(zincFieldCrossProduct, create_evaluate_2d)
+{
+	ZincTestSetupCpp zinc;
+
+	const double values1[] = { 1.0, 1.0 };
+	FieldConstant f1 = zinc.fm.createConstant(2, values1);
+	EXPECT_EQ(true, f1.isValid());
+
+	FieldCrossProduct f2 = zinc.fm.createCrossProduct(1, &f1);
+	EXPECT_EQ(true, f2.isValid());
+
+	FieldCache cache = zinc.fm.createCache();
+	double values[2];
+	EXPECT_EQ(CMISS_OK, f2.evaluateReal(cache, 2, values));
+	ASSERT_DOUBLE_EQ(-1.0, values[0]);
+	ASSERT_DOUBLE_EQ(1.0, values[1]);
+
+	// test invalid arguments
+	const double one = 1.0;
+	FieldConstant f4 = zinc.fm.createConstant(1, &one);
+	EXPECT_EQ(true, f4.isValid());
+	Field noField;
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(0, &f1).isValid());
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(1, &noField).isValid());
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(1, &f4).isValid());
+}
+
+TEST(Cmiss_field_cross_product, create_evaluate_3d)
+{
+	ZincTestSetup zinc;
+
+	const double values1[] = { 2.0, 0.0, 0.0 };
+	Cmiss_field_id f1 = Cmiss_field_module_create_constant(zinc.fm, 3, values1);
+	EXPECT_NE((Cmiss_field_id)0, f1);
+	const double values2[] = { 0.0, 1.5, 0.0 };
+	Cmiss_field_id f2 = Cmiss_field_module_create_constant(zinc.fm, 3, values2);
+	EXPECT_NE((Cmiss_field_id)0, f2);
+
+	Cmiss_field_id f3 = Cmiss_field_module_create_cross_product_3d(zinc.fm, f1, f2);
+	EXPECT_NE((Cmiss_field_id)0, f3);
+
+	Cmiss_field_cache_id cache = Cmiss_field_module_create_cache(zinc.fm);
+	double values[3];
+	EXPECT_EQ(CMISS_OK, Cmiss_field_evaluate_real(f3, cache, 3, values));
+	ASSERT_DOUBLE_EQ(0.0, values[0]);
+	ASSERT_DOUBLE_EQ(0.0, values[1]);
+	ASSERT_DOUBLE_EQ(3.0, values[2]);
+	Cmiss_field_cache_destroy(&cache);
+
+	// test invalid arguments
+	const double one = 1.0;
+	Cmiss_field_id f4 = Cmiss_field_module_create_constant(zinc.fm, 1, &one);
+	EXPECT_NE((Cmiss_field_id)0, f4);
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product_3d(0, f1, f2));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product_3d(zinc.fm, 0, f2));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product_3d(zinc.fm, f1, 0));
+	EXPECT_EQ((Cmiss_field_id)0, Cmiss_field_module_create_cross_product_3d(zinc.fm, f1, f4));
+
+	Cmiss_field_destroy(&f1);
+	Cmiss_field_destroy(&f2);
+	Cmiss_field_destroy(&f3);
+	Cmiss_field_destroy(&f4);
+}
+
+TEST(zincFieldCrossProduct, create_evaluate_3d)
+{
+	ZincTestSetupCpp zinc;
+
+	const double values1[] = { 2.0, 0.0, 0.0 };
+	FieldConstant f1 = zinc.fm.createConstant(3, values1);
+	EXPECT_EQ(true, f1.isValid());
+	const double values2[] = { 0.0, 1.5, 0.0 };
+	FieldConstant f2 = zinc.fm.createConstant(3, values2);
+	EXPECT_EQ(true, f2.isValid());
+
+	FieldCrossProduct f3 = zinc.fm.createCrossProduct(f1, f2);
+	EXPECT_EQ(true, f3.isValid());
+
+	FieldCache cache = zinc.fm.createCache();
+	double values[3];
+	EXPECT_EQ(CMISS_OK, f3.evaluateReal(cache, 3, values));
+	ASSERT_DOUBLE_EQ(0.0, values[0]);
+	ASSERT_DOUBLE_EQ(0.0, values[1]);
+	ASSERT_DOUBLE_EQ(3.0, values[2]);
+
+	// test invalid arguments
+	const double one = 1.0;
+	FieldConstant f4 = zinc.fm.createConstant(1, &one);
+	EXPECT_EQ(true, f4.isValid());
+	Field noField;
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(noField, f2).isValid());
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(f1, noField).isValid());
+	EXPECT_EQ(false, zinc.fm.createCrossProduct(f1, f4).isValid());
+}
+
+TEST(Cmiss_field_sum_components, create_evaluate)
 {
 	ZincTestSetup zinc;
 
@@ -41,7 +168,7 @@ TEST(Cmiss_field_sum_components, create)
 	Cmiss_field_destroy(&f2);
 }
 
-TEST(zincFieldSumComponents, create)
+TEST(zincFieldSumComponents, create_evaluate)
 {
 	ZincTestSetupCpp zinc;
 
