8559242b098e5fd6dcb1d65cc41c1fe69591f901
Richard Christie <r.christie@auckland.ac.nz>
2013-07-01 00:54:24 +0000

    Add unit test reading embedded nodes and their host elements from same file. Issue 3614.

--- fieldmodule/region_io.cpp
+++ fieldmodule/region_io.cpp
@@ -4,9 +4,15 @@
 #include <zinc/zincconfigure.h>
 #include <zinc/core.h>
 #include <zinc/context.h>
+#include <zinc/field.h>
+#include <zinc/fieldmodule.h>
+#include <zinc/fieldfiniteelement.h>
+#include <zinc/node.h>
 #include <zinc/region.h>
 #include <zinc/status.h>
 #include <zinc/stream.h>
+
+#include "zinctestsetup.hpp"
 
 #include "test_resources.h"
 
@@ -48,3 +54,40 @@ TEST(region_file_input, invalid_args)
 	Cmiss_context_destroy(&context);
 }
 
+TEST(issue3614, read_embedded_nodes)
+{
+	ZincTestSetup zinc;
+
+	int result = Cmiss_region_read_file(zinc.root_region,
+		TestResources::getLocation(TestResources::FIELDMODULE_EMBEDDING_ISSUE3614_RESOURCE));
+	EXPECT_EQ(CMISS_OK, result);
+
+	Cmiss_field_id coordinates = Cmiss_field_module_find_field_by_name(zinc.fm, "coordinates");
+	EXPECT_NE(static_cast<Cmiss_field_id>(0), coordinates);
+	Cmiss_field_id hostLocation = Cmiss_field_module_find_field_by_name(zinc.fm, "host_location");
+	EXPECT_NE(static_cast<Cmiss_field_id>(0), hostLocation);
+	EXPECT_EQ(CMISS_FIELD_VALUE_TYPE_MESH_LOCATION, Cmiss_field_get_value_type(hostLocation));
+	Cmiss_field_id hostCoordinates = Cmiss_field_module_create_embedded(zinc.fm, coordinates, hostLocation);
+	EXPECT_NE(static_cast<Cmiss_field_id>(0), hostLocation);
+
+	Cmiss_field_cache_id cache = Cmiss_field_module_create_cache(zinc.fm);
+
+	Cmiss_nodeset_id nodeset = Cmiss_field_module_find_nodeset_by_domain_type(zinc.fm, CMISS_FIELD_DOMAIN_NODES);
+	EXPECT_NE(static_cast<Cmiss_nodeset_id>(0), nodeset);
+	Cmiss_node_id node = Cmiss_nodeset_find_node_by_identifier(nodeset, 1003);
+	EXPECT_NE(static_cast<Cmiss_node_id>(0), node);
+	Cmiss_field_cache_set_node(cache, node);
+	double x[3] = { 0.0, 0.0, 0.0 };
+	result = Cmiss_field_evaluate_real(hostCoordinates, cache, 3, x);
+	EXPECT_EQ(CMISS_OK, result);
+	ASSERT_DOUBLE_EQ(0.25, x[0]);
+	ASSERT_DOUBLE_EQ(0.75, x[1]);
+	ASSERT_DOUBLE_EQ(0.0, x[2]);
+
+	Cmiss_node_destroy(&node);
+	Cmiss_nodeset_destroy(&nodeset);
+	Cmiss_field_cache_destroy(&cache);
+	Cmiss_field_destroy(&hostCoordinates);
+	Cmiss_field_destroy(&hostLocation);
+	Cmiss_field_destroy(&coordinates);
+}
