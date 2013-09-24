356657d03b83f56cc899f50d5d438a5d4a05a665
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-09-24 23:26:03 +0000

    Turning the custom command to create the package initialisation files into a custom target.  Also adding in a pkgutil extend path fix to allow for distributing the opencmiss packages separately, thanks to Adam Reeve for finding this.  https://tracker.physiomeproject.org/show_bug.cgi?id=3260

--- python/createpackagefiles.in.cmake
+++ python/createpackagefiles.in.cmake
@@ -1,3 +1,12 @@
 # Write the version of PyZinc into the package file __init__.py
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${OPENCMISS_PYTHON_MODULE}/__init__.py" "# OpenCMISS Python package initialisation file.\n")
-FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${ZINC_PYTHON_MODULE}/__init__.py" "# Zinc Python package initialisation file.\n\n__version__ = \"${ZINC_VERSION}\"\n\n")
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${OPENCMISS_PYTHON_MODULE}/__init__.py" "
+# OpenCMISS Python package initialisation file.
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+")
+FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${ZINC_PYTHON_MODULE}/__init__.py" "
+# Zinc Python package initialisation file.
+__version__ = \"${ZINC_VERSION}\"
+
+")
