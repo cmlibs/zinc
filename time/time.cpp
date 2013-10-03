806e88b6ffcec8e65e07547048aa53f4fde8a270
Richard Christie <r.christie@auckland.ac.nz>
2013-10-03 01:07:39 +0000

    Add zinc unit tests for new timekeeper and timenotifier APIs. Issue 3662.

---
+++ time/time.cpp
@@ -0,0 +1,81 @@
+#include <gtest/gtest.h>
+
+#include <zinc/timekeeper.h>
+#include <zinc/timenotifier.h>
+
+#include <zinc/timekeeper.hpp>
+#include <zinc/timenotifier.hpp>
+
+#include "zinctestsetup.hpp"
+#include "zinctestsetupcpp.hpp"
+
+int timeNotifierCallback(cmzn_timenotifier_id timenotifier,
+	double currentTime, void *storeTimeAddressVoid)
+{
+	EXPECT_NE(static_cast<cmzn_timenotifier_id>(0), timenotifier);
+	double *storeTimeAddress = static_cast<double *>(storeTimeAddressVoid);
+	EXPECT_NE(static_cast<double *>(0), storeTimeAddress);
+	*storeTimeAddress = currentTime;
+	return CMZN_OK;
+}
+
+typedef int (*cmzn_timenotifier_callback)(cmzn_timenotifier_id timenotifier,
+	double current_time, void *user_data);
+
+TEST(cmzn_timekeeper, api)
+{
+	ZincTestSetup zinc;
+
+	cmzn_timekeeper_id timekeeper = cmzn_context_get_default_timekeeper(zinc.context);
+	EXPECT_NE(static_cast<cmzn_timekeeper_id>(0), timekeeper);
+
+	int result;
+	EXPECT_EQ(CMZN_OK, result = cmzn_timekeeper_set_minimum_time(timekeeper, -1.0));
+	ASSERT_DOUBLE_EQ(-1.0, cmzn_timekeeper_get_minimum_time(timekeeper));
+	EXPECT_EQ(CMZN_OK, result = cmzn_timekeeper_set_maximum_time(timekeeper, 5.0));
+	ASSERT_DOUBLE_EQ(5.0, cmzn_timekeeper_get_maximum_time(timekeeper));
+	EXPECT_EQ(CMZN_OK, result = cmzn_timekeeper_set_time(timekeeper, 2.5));
+	ASSERT_DOUBLE_EQ(2.5, cmzn_timekeeper_get_time(timekeeper));
+
+	cmzn_timenotifier_id timenotifier = cmzn_timekeeper_create_timenotifier_regular(
+		timekeeper, /*update_frequency*/10, /*time_offset*/0.0);
+	EXPECT_NE(static_cast<cmzn_timenotifier_id>(0), timenotifier);
+	ASSERT_DOUBLE_EQ(2.5, cmzn_timenotifier_get_time(timenotifier));
+	double notifiedTime = 0.0;
+	EXPECT_EQ(CMZN_OK, cmzn_timenotifier_add_callback(timenotifier, timeNotifierCallback, static_cast<void *>(&notifiedTime)));
+
+	EXPECT_EQ(CMZN_OK, result = cmzn_timekeeper_set_time(timekeeper, 3.3));
+	// check callback happened
+	ASSERT_DOUBLE_EQ(3.3, notifiedTime);
+	EXPECT_EQ(CMZN_OK, cmzn_timenotifier_remove_callback(timenotifier, timeNotifierCallback, static_cast<void *>(&notifiedTime)));
+
+	EXPECT_EQ(CMZN_OK, result = cmzn_timekeeper_set_time(timekeeper, 4.7));
+	// check callback did not happen
+	ASSERT_DOUBLE_EQ(3.3, notifiedTime);
+
+	cmzn_timenotifier_destroy(&timenotifier);
+	cmzn_timekeeper_destroy(&timekeeper);
+}
+
+TEST(ZincTimekeeper, api)
+{
+	ZincTestSetupCpp zinc;
+
+	Timekeeper timekeeper = zinc.context.getDefaultTimekeeper();
+	EXPECT_TRUE(timekeeper.isValid());
+
+	int result;
+	EXPECT_EQ(CMZN_OK, result = timekeeper.setMinimumTime(-1.0));
+	ASSERT_DOUBLE_EQ(-1.0, timekeeper.getMinimumTime());
+	EXPECT_EQ(CMZN_OK, result = timekeeper.setMaximumTime(5.0));
+	ASSERT_DOUBLE_EQ(5.0, timekeeper.getMaximumTime());
+	EXPECT_EQ(CMZN_OK, result = timekeeper.setTime(2.5));
+	ASSERT_DOUBLE_EQ(2.5, timekeeper.getTime());
+
+	Timenotifier timenotifier = timekeeper.createTimenotifierRegular(
+		/*update_frequency*/10, /*time_offset*/0.0);
+	EXPECT_TRUE(timenotifier.isValid());
+	ASSERT_DOUBLE_EQ(2.5, timenotifier.getTime());
+
+	// timenotifier callbacks not yet implemented in C++
+}
