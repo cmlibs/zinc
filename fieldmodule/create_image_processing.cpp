96956a6c9a00ca88152246a10e922ceb36b6ff8e
Alan Wu <alan.wu@auckland.ac.nz>
2013-10-10 21:54:57 +0000

    Updated unit tests for image processing APIs. https://tracker.physiomeproject.org/show_bug.cgi?id=3662

--- fieldmodule/create_image_processing.cpp
+++ fieldmodule/create_image_processing.cpp
@@ -12,26 +12,26 @@
 #include <zinc/fieldimageprocessing.h>
 #include <zinc/fieldmodule.h>
 #include <zinc/region.h>
-#include <zinc/status.h>
+#include <zinc/status.h>Ä
 #include <zinc/stream.h>
 
 #include "zinctestsetup.hpp"
 #include "test_resources.h"
 
-TEST(cmzn_fieldmodule_create_field_connected_threshold_image_filter, invalid_args)
+TEST(cmzn_fieldmodule_create_field_imagefilter_connected_threshold, invalid_args)
 {
 	const double values[] = { 0.3, 0.1, 0.7 };
 	ZincTestSetup zinc;
 
-	cmzn_field_id f1 = cmzn_fieldmodule_create_field_curvature_anisotropic_diffusion_image_filter(zinc.fm, 0, 0.2, 1.0, 1);
+	cmzn_field_id f1 = cmzn_fieldmodule_create_field_imagefilter_curvature_anisotropic_diffusion(zinc.fm, 0, 0.2, 1.0, 1);
 	EXPECT_EQ((cmzn_field_id)0, f1);
 
-	cmzn_field_id f2 = cmzn_fieldmodule_create_field_connected_threshold_image_filter(zinc.fm, 0, 0.2, 1.0, 1.0, 1, 3, values);
+	cmzn_field_id f2 = cmzn_fieldmodule_create_field_imagefilter_connected_threshold(zinc.fm, 0, 0.2, 1.0, 1.0, 1, 3, values);
 	EXPECT_EQ((cmzn_field_id)0, f2);
 
 }
 
-TEST(cmzn_fieldmodule_create_field_curvature_anisotropic_diffusion_image_filter, valid_args)
+TEST(cmzn_fieldmodule_create_field_imagefilter_curvature_anisotropic_diffusion, valid_args)
 {
 	ZincTestSetup zinc;
 
@@ -47,7 +47,7 @@ TEST(cmzn_fieldmodule_create_field_curvature_anisotropic_diffusion_image_filter,
 
 	EXPECT_EQ(CMZN_OK, cmzn_field_image_read(im, si));
 
-	cmzn_field_id f2 = cmzn_fieldmodule_create_field_curvature_anisotropic_diffusion_image_filter(zinc.fm, cmzn_field_image_base_cast(im), 0.1, 1.0, 1);
+	cmzn_field_id f2 = cmzn_fieldmodule_create_field_imagefilter_curvature_anisotropic_diffusion(zinc.fm, cmzn_field_image_base_cast(im), 0.1, 1.0, 1);
 	EXPECT_NE((cmzn_field_id)0, f2);
 
 //	cmzn_field_id f2 = cmzn_fieldmodule_create_field_connected_threshold_image_filter(zinc.fm, f1, 0.2, 1.0, 1.0, 1, 3, values);
@@ -77,7 +77,7 @@ TEST(cmzn_fieldmodule_create_field_curvature_anisotropic_diffusion_image_filter,
 
 }
 
-TEST(cmzn_fieldmodule_create_field_connected_threshold_image_filter, valid_args)
+TEST(cmzn_fieldmodule_create_field_imagefilter_connected_threshold, valid_args)
 {
 	ZincTestSetup zinc;
 	double location[] = { 0.7, 0.2};
@@ -94,7 +94,7 @@ TEST(cmzn_fieldmodule_create_field_connected_threshold_image_filter, valid_args)
 
 	EXPECT_EQ(CMZN_OK, cmzn_field_image_read(im, si));
 
-	cmzn_field_id f2 = cmzn_fieldmodule_create_field_connected_threshold_image_filter(zinc.fm, cmzn_field_image_base_cast(im), 0.2, 0.22, 0.33, 1, 2, location);
+	cmzn_field_id f2 = cmzn_fieldmodule_create_field_imagefilter_connected_threshold(zinc.fm, cmzn_field_image_base_cast(im), 0.2, 0.22, 0.33, 1, 2, location);
 	EXPECT_NE((cmzn_field_id)0, f2);
 
 	cmzn_field_id xi = cmzn_field_image_get_domain_field(im);
