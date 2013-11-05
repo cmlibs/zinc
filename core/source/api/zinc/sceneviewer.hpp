5a5e773d2f7de9e9fde76ce5ce0bca7711b16e5f
Alan Wu <alan.wu@auckland.ac.nz>
2013-11-05 01:26:51 +0000

    Removed the namespace for two APIs returning sceneviewerinput. https://tracker.physiomeproject.org/show_bug.cgi?id=3260

--- core/source/api/zinc/sceneviewer.hpp
+++ core/source/api/zinc/sceneviewer.hpp
@@ -155,12 +155,12 @@ public:
 		return cmzn_sceneviewer_set_viewport_size(id, width, height);
 	}
 
-	OpenCMISS::Zinc::Sceneviewerinput createSceneviewerinput()
+	Sceneviewerinput createSceneviewerinput()
 	{
-		return OpenCMISS::Zinc::Sceneviewerinput(cmzn_sceneviewer_create_sceneviewerinput(id));
+		return Sceneviewerinput(cmzn_sceneviewer_create_sceneviewerinput(id));
 	}
 
-	int processSceneviewerinput(OpenCMISS::Zinc::Sceneviewerinput& input)
+	int processSceneviewerinput(Sceneviewerinput& input)
 	{
 		return cmzn_sceneviewer_process_sceneviewerinput(id, input.getId());
 	}
