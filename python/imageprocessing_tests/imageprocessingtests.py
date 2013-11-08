4498285fccabc23108bb3219f36519e5b1998c2a
Alan Wu <alan.wu@auckland.ac.nz>
2013-11-08 03:30:03 +0000

    Added imagefilter histogram and imagefilter mean testing to python unit test. https://tracker.physiomeproject.org/show_bug.cgi?id=3611

--- python/imageprocessing_tests/imageprocessingtests.py
+++ python/imageprocessing_tests/imageprocessingtests.py
@@ -5,15 +5,17 @@ Created on October 11, 2013
 '''
 import unittest
 
-from imageprocessing_tests import imagefilterbinarythresholdtests, imagefilterconnectedthresholdtests, imagefilterthresholdtests,imagefilterdiscretegaussiantests
+from imageprocessing_tests import imagefilterbinarythresholdtests, imagefilterconnectedthresholdtests, imagefilterhistogramtests, imagefiltermeantests, imagefilterthresholdtests, imagefilterdiscretegaussiantests
 
 def suite():
     #import ImportTestCase
     tests = unittest.TestSuite()
     tests.addTests(imagefilterbinarythresholdtests.suite())
     tests.addTests(imagefilterconnectedthresholdtests.suite())
-    tests.addTests(imagefilterthresholdtests.suite())
     tests.addTests(imagefilterdiscretegaussiantests.suite())
+    tests.addTests(imagefilterhistogramtests.suite())
+    tests.addTests(imagefiltermeantests.suite())
+    tests.addTests(imagefilterthresholdtests.suite())
     
     return tests
 
