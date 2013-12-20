97c22bd5830c31d0ead64365a7fa0faac5537492
Richard Christie <r.christie@auckland.ac.nz>
2013-12-20 03:04:40 +0000

    Change Zinc language bindings to MPL2.0 license. Issue 3558.

--- python/Functions.cmake
+++ python/Functions.cmake
@@ -1,3 +1,8 @@
+# OpenCMISS-Zinc Library
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 FUNCTION( GET_SYSTEM_NAME RETURN_SYSTEM_NAME )
 	IF( WIN32 )
