fc88ee76eedc86c5df22196a0dc3cc0968b710c8
Alan Wu <alan.wu@auckland.ac.nz>
2013-12-20 03:25:48 +0000

    Update python unit tests to adopt the fieldmoduleevent changes. https://tracker.physiomeproject.org/show_bug.cgi?id=3231

--- python/field_tests/fieldmodulenotifiertests.py
+++ python/field_tests/fieldmodulenotifiertests.py
@@ -7,9 +7,10 @@ import unittest
 
 from opencmiss.zinc.context import Context
 from opencmiss.zinc.fieldmodule import Fieldmodule, Fieldmodulenotifier, Fieldmoduleevent
+from opencmiss.zinc.field import Field
 import opencmiss.zinc.status
 
-lastfieldevent = Fieldmoduleevent.CHANGE_FLAG_NONE
+lastfieldevent = Field.CHANGE_FLAG_NONE
 
 def fieldmoduleCallback(event):
     global lastfieldevent 
@@ -40,34 +41,34 @@ class FieldmodulenotifierTestCase(unittest.TestCase):
         joe = self.field_module.createFieldConstant([2.0])
         self.assertTrue(joe.isValid())
         
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_ADD, lastfieldevent.getChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_ADD, lastfieldevent.getSummaryFieldChangeFlags())
 
         self.assertEqual(opencmiss.zinc.status.OK, joe.setName("joe"))
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_IDENTIFIER, lastfieldevent.getChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_IDENTIFIER, lastfieldevent.getSummaryFieldChangeFlags())
         
         cache = self.field_module.createFieldcache()
         self.assertEqual(opencmiss.zinc.status.OK, joe.assignReal(cache, [4.5]))
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_DEFINITION, lastfieldevent.getChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_DEFINITION | Field.CHANGE_FLAG_FULL_RESULT, lastfieldevent.getSummaryFieldChangeFlags())
         
         self.assertEqual(opencmiss.zinc.status.OK, joe.setManaged(True));
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_METADATA, lastfieldevent.getChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_DEFINITION, lastfieldevent.getSummaryFieldChangeFlags())
 
         fred = self.field_module.createFieldMagnitude(joe)
         self.assertTrue(fred.isValid())
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_ADD, lastfieldevent.getChangeFlags())
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_ADD, lastfieldevent.getFieldChangeFlags(fred))
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_NONE, lastfieldevent.getFieldChangeFlags(joe))
+        self.assertEqual(Field.CHANGE_FLAG_ADD, lastfieldevent.getSummaryFieldChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_ADD, lastfieldevent.getFieldChangeFlags(fred))
+        self.assertEqual(Field.CHANGE_FLAG_NONE, lastfieldevent.getFieldChangeFlags(joe))
 
         self.assertEqual(opencmiss.zinc.status.OK, joe.assignReal(cache, [4.5]))
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_DEFINITION | Fieldmoduleevent.CHANGE_FLAG_DEPENDENCY,
-            lastfieldevent.getChangeFlags())
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_DEFINITION, lastfieldevent.getFieldChangeFlags(joe))
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_DEPENDENCY, lastfieldevent.getFieldChangeFlags(fred))
+        self.assertEqual(Field.CHANGE_FLAG_DEFINITION | Field.CHANGE_FLAG_FULL_RESULT,
+            lastfieldevent.getSummaryFieldChangeFlags())
+        self.assertEqual(Field.CHANGE_FLAG_DEFINITION | Field.CHANGE_FLAG_FULL_RESULT, lastfieldevent.getFieldChangeFlags(joe))
+        self.assertEqual(Field.CHANGE_FLAG_FULL_RESULT, lastfieldevent.getFieldChangeFlags(fred))
 
         self.assertEqual(opencmiss.zinc.status.OK, joe.setManaged(False))
-        joe = 0
-        fred = 0
-        self.assertEqual(Fieldmoduleevent.CHANGE_FLAG_REMOVE, lastfieldevent.getChangeFlags())
+        joe = None
+        fred = None
+        self.assertEqual(Field.CHANGE_FLAG_REMOVE, lastfieldevent.getSummaryFieldChangeFlags())
 
         self.assertEqual(opencmiss.zinc.status.OK, notifier.clearCallback())
 
