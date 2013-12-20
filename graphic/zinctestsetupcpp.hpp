26e56c65746fc3e99408257c9dd270433fe637bd
Richard Christie <r.christie@auckland.ac.nz>
2013-12-20 03:55:19 +0000

    Put Zinc unit tests under MPL2.0 license. Issue 3558.

--- graphic/zinctestsetupcpp.hpp
+++ graphic/zinctestsetupcpp.hpp
@@ -1,3 +1,11 @@
+/*
+ * OpenCMISS-Zinc Library Unit Tests
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef __ZINCTESTSETUPCPP_HPP__
 #define __ZINCTESTSETUPCPP_HPP__
 
--- selection/tests.cmake
+++ selection/tests.cmake
@@ -1,3 +1,8 @@
+# OpenCMISS-Zinc Library Unit Tests
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 SET(CURRENT_TEST selection)
 LIST(APPEND API_TESTS ${CURRENT_TEST})
