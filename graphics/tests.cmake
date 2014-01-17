eecea289be6b2802ac00935c734e32f2ae051678
Richard Christie <r.christie@auckland.ac.nz>
2014-01-17 02:28:23 +0000

    Rename zinc graphics unit tests folder, from 'graphic'. https://tracker.physiomeproject.org/show_bug.cgi?id=3535

--- graphics/tests.cmake
+++ graphics/tests.cmake
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-SET(CURRENT_TEST graphic)
+SET(CURRENT_TEST graphics)
 LIST(APPEND API_TESTS ${CURRENT_TEST})
 SET(${CURRENT_TEST}_SRC
     ${CURRENT_TEST}/contours.cpp
