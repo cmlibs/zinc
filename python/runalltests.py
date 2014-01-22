9a0f9bc2363f02fc644e99cead5ba79df31d0d78
Alan Wu <alan.wu@auckland.ac.nz>
2014-01-22 02:38:10 +0000

    Added license block in python unit tests. https://tracker.physiomeproject.org/show_bug.cgi?id=3558

--- python/runalltests.py
+++ python/runalltests.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+"""
+PyZinc Unit Tests
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
 import unittest
 
 def suite():
