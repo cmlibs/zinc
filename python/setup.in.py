1fc5b383205a3395ce706f789c8b9ba4567d9eaf
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-03-04 03:22:36 +0000

    Setting specific names of the shared object library glob for different oses.  Adding the name of the zinc library to the setup.py file so that the library get installed when using the 'python setup.py install' command.  https://tracker.physiomeproject.org/show_bug.cgi?id=3709

--- python/setup.in.py
+++ python/setup.in.py
@@ -1,8 +1,8 @@
 """ Zinc Library: An advanced visualisation library for FE models
 
 The Zinc library is an advanced visualisation library for FE models.  The
-Zinc library understands a representation of mathematical fields, including 
-finite element, image-based and CAD.  It also understands fields derived by 
+Zinc library understands a representation of mathematical fields, including
+finite element, image-based and CAD.  It also understands fields derived by
 mathematical operators.
 """
 
@@ -20,17 +20,17 @@ from distutils.core import setup
 doclines = __doc__.split("\n")
 
 setup(
-    name='@PYPI_PACKAGE_NAME@',
-    version='@PYPI_SOURCE_TARGZ_VERSION@',
-    author='H. Sorby',
-    author_email='h.sorby@auckland.ac.nz',
-    packages=['opencmiss', 'opencmiss.zinc'],
-    package_data={'opencmiss.zinc': ['*.so', '*.pyd']},
-    platforms=['any'],
-    url='http://pypi.python.org/pypi/PyZinc/',
-    license='LICENSE.txt',
-    description=doclines[0],
-    classifiers = filter(None, classifiers.split("\n")),
-    long_description=open('README.txt').read(),
+	name='@PYPI_PACKAGE_NAME@',
+	version='@PYPI_SOURCE_TARGZ_VERSION@',
+	author='H. Sorby',
+	author_email='h.sorby@auckland.ac.nz',
+	packages=['opencmiss', 'opencmiss.zinc'],
+	package_data={'opencmiss.zinc': ['@ZINC_SHARED_OBJECT_GLOB@', '@ZINC_LIBRARY_NAME@']},
+	platforms=['any'],
+	url='http://pypi.python.org/pypi/PyZinc/',
+	license='LICENSE.txt',
+	description=doclines[0],
+	classifiers = filter(None, classifiers.split("\n")),
+	long_description=open('README.txt').read(),
 )
 
