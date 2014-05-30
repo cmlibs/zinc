0fb500583299180c632697fae8e0a23eb27f1d68
Richard Christie <r.christie@auckland.ac.nz>
2014-05-30 04:13:07 +0000

    Modify optimisation unit test to use conditional field to exclude DOFs from the solution. Add tests for optimisation field arguments. https://tracker.physiomeproject.org/show_bug.cgi?id=3715

--- optimisation/optimisation.cpp
+++ optimisation/optimisation.cpp
@@ -21,6 +21,8 @@
 #include <zinc/fieldcomposite.hpp>
 #include <zinc/fieldconstant.hpp>
 #include <zinc/fieldderivatives.hpp>
+#include <zinc/fieldfiniteelement.hpp>
+#include <zinc/fieldlogicaloperators.hpp>
 #include <zinc/fieldmatrixoperators.hpp>
 #include <zinc/fieldmeshoperators.hpp>
 #include <zinc/fieldnodesetoperators.hpp>
@@ -29,7 +31,7 @@
 
 #include "test_resources.h"
 
-TEST(cmzn_optimisation, valid_args)
+TEST(cmzn_optimisation, arguments)
 {
 	ZincTestSetup zinc;
 	int result;
@@ -44,7 +46,7 @@ TEST(cmzn_optimisation, valid_args)
 	cmzn_optimisation_destroy(&optimisation);
 }
 
-TEST(ZincOptimisation, valid_args)
+TEST(ZincOptimisation, arguments)
 {
 	ZincTestSetupCpp zinc;
 	int result;
@@ -55,10 +57,79 @@ TEST(ZincOptimisation, valid_args)
 	EXPECT_EQ(Optimisation::METHOD_QUASI_NEWTON, optimisation.getMethod());
 	EXPECT_EQ(OK, result = optimisation.setMethod(Optimisation::METHOD_LEAST_SQUARES_QUASI_NEWTON));
 	EXPECT_EQ(Optimisation::METHOD_LEAST_SQUARES_QUASI_NEWTON, optimisation.getMethod());
+
+	// made-up fields to test objective/independent field APIs
+	FieldFiniteElement f1 = zinc.fm.createFieldFiniteElement(3);
+	EXPECT_TRUE(f1.isValid());
+	FieldFiniteElement f2 = zinc.fm.createFieldFiniteElement(1);
+	EXPECT_TRUE(f2.isValid());
+	FieldFiniteElement f3 = zinc.fm.createFieldFiniteElement(3);
+	EXPECT_TRUE(f3.isValid());
+	Field f4 = zinc.fm.createFieldMultiply(f1, f2);
+	EXPECT_TRUE(f4.isValid());
+	Field f5 = zinc.fm.createFieldMultiply(f4, f3);
+	EXPECT_TRUE(f5.isValid());
+	const double oneValue = 1.0;
+	Field fcond = zinc.fm.createFieldConstant(1, &oneValue);
+	EXPECT_TRUE(fcond.isValid());
+
+	Field temp;
+	EXPECT_EQ(OK, result = optimisation.addIndependentField(f1));
+	EXPECT_EQ(OK, result = optimisation.addIndependentField(f2));
+	EXPECT_EQ(OK, result = optimisation.addIndependentField(f3));
+	EXPECT_EQ(ERROR_ARGUMENT, result = optimisation.addIndependentField(f1));
+	temp = optimisation.getFirstIndependentField();
+	EXPECT_EQ(f1, temp);
+	temp = optimisation.getNextIndependentField(temp);
+	EXPECT_EQ(f2, temp);
+	temp = optimisation.getNextIndependentField(temp);
+	EXPECT_EQ(f3, temp);
+	temp = optimisation.getNextIndependentField(temp);
+	EXPECT_FALSE(temp.isValid());
+	temp = optimisation.getConditionalField(f1);
+	EXPECT_FALSE(temp.isValid());
+	temp = optimisation.getConditionalField(f2);
+	EXPECT_FALSE(temp.isValid());
+	temp = optimisation.getConditionalField(f3);
+	EXPECT_FALSE(temp.isValid());
+	EXPECT_EQ(ERROR_ARGUMENT, result = optimisation.setConditionalField(f2, f3)); // wrong #components
+	EXPECT_EQ(OK, result = optimisation.setConditionalField(f2, fcond));
+	temp = optimisation.getConditionalField(f2);
+	EXPECT_EQ(fcond, temp);
+
+	EXPECT_EQ(ERROR_ARGUMENT, result = optimisation.removeIndependentField(f4)); // not in use
+	EXPECT_EQ(OK, result = optimisation.removeIndependentField(f1));
+	temp = optimisation.getFirstIndependentField();
+	EXPECT_EQ(f2, temp);
+	temp = optimisation.getNextIndependentField(temp);
+	EXPECT_EQ(f3, temp);
+	temp = optimisation.getNextIndependentField(temp);
+	EXPECT_FALSE(temp.isValid());
+	temp = optimisation.getConditionalField(f2);
+	EXPECT_EQ(fcond, temp);
+	EXPECT_EQ(OK, result = optimisation.setConditionalField(f2, Field()));
+	temp = optimisation.getConditionalField(f2);
+	EXPECT_FALSE(temp.isValid());
+
+	EXPECT_EQ(OK, result = optimisation.addObjectiveField(f4));
+	EXPECT_EQ(OK, result = optimisation.addObjectiveField(f5));
+	temp = optimisation.getFirstObjectiveField();
+	EXPECT_EQ(f4, temp);
+	temp = optimisation.getNextObjectiveField(temp);
+	EXPECT_EQ(f5, temp);
+	temp = optimisation.getNextObjectiveField(temp);
+	EXPECT_FALSE(temp.isValid());
+
+	EXPECT_EQ(ERROR_ARGUMENT, result = optimisation.removeObjectiveField(f1));
+	EXPECT_EQ(OK, result = optimisation.removeObjectiveField(f5));
+	temp = optimisation.getFirstObjectiveField();
+	EXPECT_EQ(f4, temp);
+	temp = optimisation.getNextObjectiveField(temp);
+	EXPECT_FALSE(temp.isValid());
 }
 
 // A non-linear optimisation for deformation of a tricubic Lagrange unit cube.
-// All nodes on end1 are fixed in x,y,z by a penalty
+// All nodes on end1 are fixed in x,y,z by being excluded as DOFs
 // All nodes on end2 are fixed to x=1.5 by a penalty
 // Incompressible and strain penalties are applied at Gauss points.
 // The solution proves that the cube gets thinner, symmetrically at end2 as it extends
@@ -80,17 +151,7 @@ TEST(ZincOptimisation, tricubicFit)
 	zinc.fm.beginChange();
 	Nodeset nodeset = zinc.fm.findNodesetByFieldDomainType(Field::DOMAIN_TYPE_NODES);
 	EXPECT_TRUE(nodeset.isValid());
-	const int end1NodeIdentifiers[] = { 1,5,9,13,17,21,25,29,33,37,41,45,49,53,57,61 };
-	const int end1NodeIdentifiersSize = sizeof(end1NodeIdentifiers)/sizeof(int);
-	FieldNodeGroup end1NodeGroup = zinc.fm.createFieldNodeGroup(nodeset);
-	NodesetGroup end1Nodeset = end1NodeGroup.getNodesetGroup();
-	EXPECT_TRUE(end1Nodeset.isValid());
-	for (int i = 0; i < end1NodeIdentifiersSize; ++i)
-	{
-		Node node = nodeset.findNodeByIdentifier(end1NodeIdentifiers[i]);
-		EXPECT_TRUE(node.isValid());
-		EXPECT_EQ(OK, result = end1Nodeset.addNode(node));
-	}
+
 	const int end2NodeIdentifiers[] = { 4,8,12,16,20,24,28,32,36,40,44,48,52,56,60,64 };
 	const int end2NodeIdentifiersSize = sizeof(end2NodeIdentifiers)/sizeof(int);
 	FieldNodeGroup end2NodeGroup = zinc.fm.createFieldNodeGroup(nodeset);
@@ -108,8 +169,6 @@ TEST(ZincOptimisation, tricubicFit)
 
 	const double penalty1Value = 10.0;
 	FieldConstant penalty1 = zinc.fm.createFieldConstant(1, &penalty1Value);
-	FieldNodesetSumSquares end1Objective = zinc.fm.createFieldNodesetSumSquares(penalty1*displacement, end1Nodeset);
-	EXPECT_TRUE(end1Objective.isValid());
 
 	FieldComponent x = zinc.fm.createFieldComponent(coordinates, 1);
 	const double xEndValue = 1.5;
@@ -135,13 +194,19 @@ TEST(ZincOptimisation, tricubicFit)
 	const int numGaussPoints = 4;
 	EXPECT_EQ(OK, result = WObjective.setNumbersOfPoints(1, &numGaussPoints));
 
+	const double xConstValue = 0.1;
+	FieldConstant xConst = zinc.fm.createFieldConstant(1, &xConstValue);
+	EXPECT_TRUE(xConst.isValid());
+	Field xCondition = x > xConst;
+	EXPECT_TRUE(xCondition.isValid());
+
 	Optimisation optimisation = zinc.fm.createOptimisation();
 	EXPECT_TRUE(optimisation.isValid());
 
-	EXPECT_EQ(OK, result = optimisation.addObjectiveField(end1Objective));
 	EXPECT_EQ(OK, result = optimisation.addObjectiveField(end2Objective));
 	EXPECT_EQ(OK, result = optimisation.addObjectiveField(WObjective));
 	EXPECT_EQ(OK, result = optimisation.addIndependentField(coordinates));
+	EXPECT_EQ(OK, result = optimisation.setConditionalField(coordinates, xCondition));
 	EXPECT_EQ(OK, result = optimisation.setMethod(Optimisation::METHOD_LEAST_SQUARES_QUASI_NEWTON));
 	EXPECT_EQ(OK, result = optimisation.setAttributeInteger(Optimisation::ATTRIBUTE_MAXIMUM_ITERATIONS, 10));
 
@@ -150,7 +215,7 @@ TEST(ZincOptimisation, tricubicFit)
 	EXPECT_NE((char *)0, solutionReport);
 	printf("%s", solutionReport);
 	const char *dimensionText = strstr(solutionReport,
-		"Dimension of the problem  = 192");
+		"Dimension of the problem  = 144");
 	EXPECT_NE((const char *)0, dimensionText);
 	const char *convergedText = strstr(solutionReport,
 		"Return code               = 2 (Algorithm converged - Difference in successive fcn values is less than tolerance)");
@@ -163,17 +228,18 @@ TEST(ZincOptimisation, tricubicFit)
 	EXPECT_TRUE(cache.isValid());
 	const double expectedWValues[10] =
 	{
-		0.011447361785109545,
-		1.1123649670134983e-006,
-		1.1123718443471489e-006,
-		0.00039367551802260676,
-		0.0014073263139825962,
-		5.6615827265801324e-006,
-		0.00039367550896460436,
-		5.6615832637350045e-006,
-		0.0014073262275251103,
-		2.4282421107936186e-006
+		0.011449513901451864,
+		1.1196820095409539e-006,
+		1.1196845955486078e-006,
+		0.00039399120829730822,
+		0.0014075399963259518,
+		5.6687058307447950e-006,
+		0.00039399120475952152,
+		5.6687063105913627e-006,
+		0.0014075399951463652,
+		2.4300383826273377e-006
 	};
+
 	double WValues[10];
 	const double tolerance = 1.0E-12;
 	EXPECT_EQ(OK, result = WObjective.evaluateReal(cache, 10, WValues));
@@ -182,7 +248,7 @@ TEST(ZincOptimisation, tricubicFit)
 
 	FieldMeshIntegral volume = zinc.fm.createFieldMeshIntegral(one, coordinates, mesh3d);
 	EXPECT_EQ(OK, volume.setNumbersOfPoints(1, &numGaussPoints));
-	const double expectedVolumeValue = 1.0001085604484732;
+	const double expectedVolumeValue = 1.0001085833573624;
 	double volumeValue;
 	EXPECT_EQ(OK, result = volume.evaluateReal(cache, 1, &volumeValue));
 	EXPECT_NEAR(expectedVolumeValue, volumeValue, tolerance);
