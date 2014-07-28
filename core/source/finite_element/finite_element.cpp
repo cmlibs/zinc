cmlibs/zinc

commit e76931eb430c427fa53f8ca375ecc242455f8fb6

Implement subelement handling for APIs removing elements from element group field / mesh group. Clarify documentation of subelement handling mode 'FULL' that on removing an element any lower dimensional subelements not in use by remaining peer elements are removed. Add new error codes ALREADY EXISTS and NOT FOUND for reporting non-success of group add/remove methods when object already in/not in group. Add 'selection changed' flag to scene to ensure change or removal of selection group results in redraw; previously last unselected node/element in cmgui was left highlighted. Add MeshGroup and NodesetGroup default constructors. Tidy up region destroy node list function implementation.   https://tracker.physiomeproject.org/show_bug.cgi?id=3745
git-svn-id: https://svn.physiomeproject.org/svn/cmiss/zinc/library/trunk@10835 3c1deb5b-d424-0410-962d-aba41a686d42

core/source/finite_element/finite_element.cpp
@@ -21932,14 +21932,6 @@ column of the <coordinate_transformation> matrix.
 int adjacent_FE_element(struct FE_element *element,
 	int face_number, int *number_of_adjacent_elements,
 	struct FE_element ***adjacent_elements)
-/*******************************************************************************
-LAST MODIFIED : 27 October 2000
-
-DESCRIPTION :
-Returns the list of <adjacent_elements> not including <element> which share the
-face indicated by <face_number>.  <adjacent_elements> is ALLOCATED to the
-correct size and should be DEALLOCATED when finished with.
-==============================================================================*/
 {
 	int i, j, return_code;
 	struct FE_element *face;
@@ -21947,29 +21939,19 @@ correct size and should be DEALLOCATED when finished with.
 	ENTER(adjacent_FE_element);
 	if ((element) && (element->shape) && (element->faces))
 	{
+		j = 0;
 		if ((0 <= face_number) && (face_number < element->shape->number_of_faces)&&
-			 (NULL != (face = (element->faces)[face_number])) && (face->parents))
+				(NULL != (face = (element->faces)[face_number])) && (face->parents))
 		{
 			if (ALLOCATE(*adjacent_elements, struct FE_element *, face->number_of_parents))
 			{
-				j = 0;
-				for (i = 0; i < face->number_of_parents; i++)
+				for (i = 0; i < face->number_of_parents; ++i)
 				{
 					if ((face->parents[i]) && (face->parents[i] != element))
 					{
 						(*adjacent_elements)[j] = face->parents[i];
-						j++;
+						++j;
 					}
-				}
-				*number_of_adjacent_elements = j;
-				if (j > 0)
-				{
-					return_code = 1;
-				}
-				else
-				{
-					DEALLOCATE(*adjacent_elements);
-					return_code = 0;
 				}
 			}
 			else
@@ -21978,13 +21960,11 @@ correct size and should be DEALLOCATED when finished with.
 					"adjacent_FE_element.  Unable to allocate array");
 				return_code = 0;
 			}
+			return_code = 1;
 		}
 		else
-		{
-			/* In the generic heart mesh there are elements that
-				do not have faces defined this isn't an error */
-			return_code = 0;
-		}
+			*adjacent_elements = 0;
+		*number_of_adjacent_elements = j;
 	}
 	else
 	{
@@ -25002,41 +24982,17 @@ Can be used to determine if a face is in use by more than one parent elements.
 	return (return_code);
 } /* get_FE_element_number_of_parents */
 
-int get_FE_element_number_of_parents_in_list(struct FE_element *element,
-	struct LIST(FE_element) *element_list, int *number_of_parents_address)
-/*******************************************************************************
-LAST MODIFIED : 14 January 2003
-
-DESCRIPTION :
-Returns the number of parents of <element> that are in <element_list>.
-==============================================================================*/
+bool cmzn_element_has_parent_in_list(cmzn_element *element,
+	LIST(cmzn_element) *elementList)
 {
-	int count, i, return_code;
-
-	ENTER(get_FE_element_number_of_parents_in_list);
-	if ((element) && (element_list) && (number_of_parents_address))
+	if ((element) && (elementList))
 	{
-		count = 0;
-		for (i = 0; i < element->number_of_parents; i++)
-		{
-			if (IS_OBJECT_IN_LIST(FE_element)(element->parents[i], element_list))
-			{
-				count++;
-			}
-		}
-		*number_of_parents_address = count;
-		return_code = 1;
+		for (int i = 0; i < element->number_of_parents; i++)
+			if (IS_OBJECT_IN_LIST(cmzn_element)(element->parents[i], elementList))
+				return true;
 	}
-	else
-	{
-		display_message(ERROR_MESSAGE,
-			"get_FE_element_number_of_parents_in_list.  Invalid argument(s)");
-		return_code = 0;
-	}
-	LEAVE;
-
-	return (return_code);
-} /* get_FE_element_number_of_parents_in_list */
+	return false;
+}
 
 int FE_element_get_first_parent(struct FE_element *element,
 	struct FE_element **parent_element_address, int *face_number_address)
@@ -28692,137 +28648,110 @@ static int FE_element_find_inherit_elements(struct FE_element *element,
 	return (return_code);
 } /* FE_element_find_inherit_elements */
 
-int FE_element_add_nodes_to_list(struct FE_element *element, void *data_void)
-/*******************************************************************************
-LAST MODIFIED : 26 February 2003
-
-DESCRIPTION :
-Adds all the nodes used by <element> to the <node_list> in the data. Since it
-is very inefficient to determine the nodes in use by faces or lines, the
-<element_list> being iterated over is also passed to this function so that only
-faces or lines with parents not in the <element_list> are checked, and only
-nodes inherited from top level elements not in the list are considered.
-The optional <intersect_node_list> restricts nodes added to <node_list> to also
-be in it.
-<data_void> points at a struct FE_element_add_nodes_to_list_data.
-==============================================================================*/
+int cmzn_element_add_stored_nodes_to_list(cmzn_element *element,
+	LIST(cmzn_node) *nodeList, LIST(cmzn_node) *onlyFromNodeList)
 {
-	int i, number_of_nodes, return_code;
-	struct FE_element *inherit_element;
-	struct FE_element_add_nodes_to_list_data *data;
-	struct FE_node **element_field_nodes, **nodes;
-	struct LIST(FE_element) *inherit_element_list;
-
-	ENTER(FE_element_add_nodes_to_list);
-	if (element && (data = (struct FE_element_add_nodes_to_list_data *)data_void))
+	if (!(element && nodeList && onlyFromNodeList))
+		return CMZN_ERROR_ARGUMENT;
+	int return_code = CMZN_OK;
+	if ((element->information) && (element->information->nodes))
 	{
-		return_code = 1;
-		if (element->information)
+		cmzn_node *node;
+		const int localNodesCount = element->information->number_of_nodes;
+		for (int i = 0; i < localNodesCount; i++)
 		{
-			if ((0 < (number_of_nodes = element->information->number_of_nodes)) &&
-				(nodes = element->information->nodes))
+			node = element->information->nodes[i];
+			if (node && IS_OBJECT_IN_LIST(cmzn_node)(node, onlyFromNodeList))
 			{
-				for (i = number_of_nodes; (0 < i) && return_code; i--)
+				if (!(ADD_OBJECT_TO_LIST(FE_node)(node, nodeList) ||
+					IS_OBJECT_IN_LIST(FE_node)(node, nodeList)))
 				{
-					if (*nodes)
-					{
-						if (!IS_OBJECT_IN_LIST(FE_node)(*nodes, data->node_list))
-						{
-							if ((!(data->intersect_node_list)) ||
-								IS_OBJECT_IN_LIST(FE_node)(*nodes, data->intersect_node_list))
-							{
-								return_code =
-									ADD_OBJECT_TO_LIST(FE_node)(*nodes, data->node_list);
-							}
-						}
-					}
-					nodes++;
-				}
-			}
-		}
-		if (return_code)
-		{
-			int check_parents;
-
-			/* only look at parents if there are any NOT in element_list */
-			check_parents = 0;
-			for (i = 0; i < element->number_of_parents; i++)
-			{
-				if (!IS_OBJECT_IN_LIST(FE_element)(element->parents[i], data->element_list))
-				{
-					check_parents = 1;
+					return_code = CMZN_ERROR_GENERAL;
 					break;
 				}
 			}
-			if (check_parents)
+		}
+	}
+	return return_code;
+}
+
+int cmzn_element_add_nodes_to_list(cmzn_element *element, LIST(cmzn_node) *nodeList)
+{
+	if (!(element && nodeList))
+		return CMZN_ERROR_ARGUMENT;
+	int return_code = CMZN_OK;
+	cmzn_node *node;
+	if ((element->information) && (element->information->nodes))
+	{
+		const int localNodesCount = element->information->number_of_nodes;
+		for (int i = 0; i < localNodesCount; i++)
+		{
+			node = element->information->nodes[i];
+			if ((node) && (!(ADD_OBJECT_TO_LIST(FE_node)(node, nodeList) ||
+				IS_OBJECT_IN_LIST(FE_node)(node, nodeList))))
 			{
-				/* get list of node-containing elements that are ancestors of this
-					 element, are not themselves in the data->element_list and with
-					 intermediate ancestors not in the data->element_list */
-				inherit_element_list = CREATE(LIST(FE_element))();
-				return_code = FE_element_find_inherit_elements(element,
-					data->element_list, inherit_element_list);
-				if (return_code)
-				{
-					while (return_code && (inherit_element =
-						FIRST_OBJECT_IN_LIST_THAT(FE_element)(
-							(LIST_CONDITIONAL_FUNCTION(FE_element) *)NULL, (void *)NULL,
-							inherit_element_list)))
-					{
-						return_code = REMOVE_OBJECT_FROM_LIST(FE_element)(
-							inherit_element, inherit_element_list);
-						if (return_code)
-						{
-							/*???RC Just handle first coordinate field for now -- later
-								check ALL fields! */
-							return_code = calculate_FE_element_field_nodes(element,
-								(struct FE_field *)NULL, &number_of_nodes, &element_field_nodes,
-								inherit_element);
-							if (return_code)
-							{
-								if (NULL != (nodes = element_field_nodes))
-								{
-									for (i = number_of_nodes; 0 < i; i--)
-									{
-										if (*nodes)
-										{
-											if (!IS_OBJECT_IN_LIST(FE_node)(*nodes, data->node_list))
-											{
-												if ((!(data->intersect_node_list)) ||
-													IS_OBJECT_IN_LIST(FE_node)(*nodes,
-														data->intersect_node_list))
-												{
-													if (!ADD_OBJECT_TO_LIST(FE_node)(*nodes,
-														data->node_list))
-													{
-														return_code = 0;
-													}
-												}
-											}
-											DEACCESS(FE_node)(nodes);
-										}
-										nodes++;
-									}
-									DEALLOCATE(element_field_nodes);
-								}
-							}
-						}
-					}
-				}
-				DESTROY(LIST(FE_element))(&inherit_element_list);
+				return_code = CMZN_ERROR_GENERAL;
+				break;
 			}
 		}
 	}
-	else
+	if (0 < element->number_of_parents)
 	{
-		display_message(ERROR_MESSAGE,
-			"FE_element_add_nodes_to_list.  Invalid argument(s)");
-		return_code = 0;
+		int number_of_element_field_nodes;
+		cmzn_node_id *element_field_nodes_array;
+		if (calculate_FE_element_field_nodes(element, (struct FE_field *)NULL,
+			&number_of_element_field_nodes, &element_field_nodes_array,
+			/*top_level_element*/(struct FE_element *)NULL))
+		{
+			for (int i = 0; i < number_of_element_field_nodes; i++)
+			{
+				node = element_field_nodes_array[i];
+				if ((node) && (!(ADD_OBJECT_TO_LIST(FE_node)(node, nodeList) ||
+						IS_OBJECT_IN_LIST(FE_node)(node, nodeList))))
+					return_code = CMZN_ERROR_GENERAL;
+				cmzn_node_destroy(&node);
+			}
+			DEALLOCATE(element_field_nodes_array);
+		}
+		else
+			return_code = CMZN_ERROR_GENERAL;
 	}
-	LEAVE;
+	return return_code;
+}
 
-	return (return_code);
-} /* FE_element_add_nodes_to_list */
+int cmzn_element_remove_nodes_from_list(cmzn_element *element, LIST(cmzn_node) *nodeList)
+{
+	if (!(element && nodeList))
+		return CMZN_ERROR_ARGUMENT;
+	int return_code = CMZN_OK;
+	cmzn_node *node;
+	if ((element->information) && (element->information->nodes))
+	{
+		const int localNodesCount = element->information->number_of_nodes;
+		for (int i = 0; i < localNodesCount; i++)
+			REMOVE_OBJECT_FROM_LIST(cmzn_node)(element->information->nodes[i], nodeList);
+	}
+	if (0 < element->number_of_parents)
+	{
+		int number_of_element_field_nodes;
+		cmzn_node_id *element_field_nodes_array;
+		if (calculate_FE_element_field_nodes(element, (struct FE_field *)NULL,
+			&number_of_element_field_nodes, &element_field_nodes_array,
+			/*top_level_element*/(struct FE_element *)NULL))
+		{
+			for (int i = 0; i < number_of_element_field_nodes; i++)
+			{
+				node = element_field_nodes_array[i];
+				REMOVE_OBJECT_FROM_LIST(cmzn_node)(element_field_nodes_array[i], nodeList);
+				cmzn_node_destroy(&node);
+			}
+			DEALLOCATE(element_field_nodes_array);
+		}
+		else
+			return_code = CMZN_ERROR_GENERAL;
+	}
+	return return_code;
+}
 
 int FE_element_is_dimension(struct FE_element *element,void *dimension_void)
 /*******************************************************************************

core/source/api/zinc/element.h
@@ -350,7 +350,9 @@ ZINC_C_INLINE cmzn_mesh_id cmzn_mesh_group_base_cast(
  *
  * @param mesh_group  Handle to mesh group to modify.
  * @param element  Handle to element to add. Must be from the group's master mesh.
- * @return  Status CMZN_OK on success, any other value on failure.
+ * @return  Status CMZN_OK on success, CMZN_ERROR_ALREADY_EXISTS if element
+ * was already in the group but otherwise successful, or any other value on
+ * more serious failure.
  */
 ZINC_API int cmzn_mesh_group_add_element(cmzn_mesh_group_id mesh_group,
 	cmzn_element_id element);
@@ -382,7 +384,9 @@ ZINC_API int cmzn_mesh_group_remove_all_elements(cmzn_mesh_group_id mesh_group);
  *
  * @param mesh_group  Handle to mesh group to modify.
  * @param element  Handle to element to remove.
- * @return  Status CMZN_OK if element removed, any other value if failed.
+ * @return  Status CMZN_OK on success, CMZN_ERROR_NOT_FOUND if element was not
+ * in the group but otherwise successful, or any other value on more serious
+ * failure.
  */
 ZINC_API int cmzn_mesh_group_remove_element(cmzn_mesh_group_id mesh_group,
 	cmzn_element_id element);

core/source/api/zinc/element.hpp
@@ -538,6 +538,9 @@ public:
 	explicit MeshGroup(cmzn_mesh_group_id mesh_id) : Mesh(reinterpret_cast<cmzn_mesh_id>(mesh_id))
 	{ }
 
+	MeshGroup()
+	{ }
+
 	cmzn_mesh_group_id getId() const
 	{
 		return (cmzn_mesh_group_id)(id);

core/source/api/zinc/node.h
@@ -270,7 +270,9 @@ ZINC_C_INLINE cmzn_nodeset_id cmzn_nodeset_group_base_cast(
  *
  * @param nodeset_group  Handle to nodeset group to modify.
  * @param node  Handle to node to add. Must be from the group's master nodeset.
- * @return  Status CMZN_OK on success, any other value on failure.
+ * @return  Status CMZN_OK on success, CMZN_ERROR_ALREADY_EXISTS if node was
+ * already in group but otherwise successful, or any other value on more
+ * serious failure.
  */
 ZINC_API int cmzn_nodeset_group_add_node(cmzn_nodeset_group_id nodeset_group,
 	cmzn_node_id node);
@@ -300,7 +302,9 @@ ZINC_API int cmzn_nodeset_group_remove_all_nodes(cmzn_nodeset_group_id nodeset_g
  *
  * @param nodeset_group  Handle to nodeset group to modify.
  * @param node  Handle to node to remove.
- * @return  Status CMZN_OK if node is removed, any other value if failed.
+ * @return  Status CMZN_OK on success, CMZN_ERROR_NOT_FOUND if node
+ * was not in the group but otherwise successful, or any other value
+ * on more serious failure.
  */
 ZINC_API int cmzn_nodeset_group_remove_node(cmzn_nodeset_group_id nodeset_group,
 	cmzn_node_id node);

core/source/api/zinc/node.hpp
@@ -380,6 +380,9 @@ public:
 	explicit NodesetGroup(cmzn_nodeset_group_id nodeset_id) : Nodeset(reinterpret_cast<cmzn_nodeset_id>(nodeset_id))
 	{ }
 
+	NodesetGroup()
+	{ }
+
 	cmzn_nodeset_group_id getId() const
 	{
 		return (cmzn_nodeset_group_id)(id);

core/source/api/zinc/status.h
@@ -30,6 +30,10 @@ extern "C" {
  */
 enum cmzn_status
 {
+	CMZN_ERROR_ALREADY_EXISTS = -4,
+		/*!< Object already exists or is present in owner/set */
+	CMZN_ERROR_NOT_FOUND = -3,
+		/*!< Object or resource not found */
 	CMZN_ERROR_MEMORY = -2,
 		/*!< Failed to allocate memory. */
 	CMZN_ERROR_ARGUMENT = -1,

core/source/api/zinc/status.hpp
@@ -32,6 +32,10 @@ namespace Zinc
  */
 enum Status
 {
+	ERROR_ALREADY_EXISTS = CMZN_ERROR_ALREADY_EXISTS,
+		/*!< Object already exists or is present in owner/set */
+	ERROR_NOT_FOUND = CMZN_ERROR_NOT_FOUND,
+		/*!< Object or resource not found */
 	ERROR_MEMORY = CMZN_ERROR_MEMORY,
 		/*!< Failed to allocate memory. */
 	ERROR_ARGUMENT = CMZN_ERROR_ARGUMENT,

core/source/api/zinc/types/fieldgroupid.h
@@ -37,7 +37,9 @@ enum cmzn_field_group_subelement_handling_mode
 	 * This is the default mode for new groups. */
 	CMZN_FIELD_GROUP_SUBELEMENT_HANDLING_MODE_FULL = 2
 	/*!< Ensure group contains all faces, lines and nodes for any higher dimension
-	 * element in the wider group. More expensive to maintain. */
+	 * element added to the group; on removal of an element remove any of its
+	 * faces, lines and nodes not part of any neighbour element.
+	 * More expensive to maintain. */
 };
 
 #endif

commit 03bf4347fd134cc9a9dfc198cde214f70473f4bd

Fixing an uninitialised return code caused by previous commit r10835. https://tracker.physiomeproject.org/show_bug.cgi?id=3745
git-svn-id: https://svn.physiomeproject.org/svn/cmiss/zinc/library/trunk@10869 3c1deb5b-d424-0410-962d-aba41a686d42

core/source/finite_element/finite_element.cpp
@@ -21963,7 +21963,10 @@ int adjacent_FE_element(struct FE_element *element,
 			return_code = 1;
 		}
 		else
+		{
 			*adjacent_elements = 0;
+			return_code = 0;
+		}
 		*number_of_adjacent_elements = j;
 	}
 	else