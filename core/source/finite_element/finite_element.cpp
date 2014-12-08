9cf1c094ae5533b12ec2235ef6e9315bcd282a5b
Richard Christie <r.christie@auckland.ac.nz>
2014-12-08 03:18:40 +0000

    Fix compilation errors from revision C11001. Also fix some warnings in finite_element.cpp.  https://tracker.physiomeproject.org/show_bug.cgi?id=3827

--- core/source/finite_element/finite_element.cpp
+++ core/source/finite_element/finite_element.cpp
@@ -485,7 +485,7 @@ public:
 	 * Default implementation is for non-node-based map.
 	 * @return  true if successfully evaluated, otherwise false.
 	 */
-	virtual bool evaluateNode(ElementDOFMapEvaluationCache& cache, FE_node*& node)
+	virtual bool evaluateNode(ElementDOFMapEvaluationCache& /*cache*/, FE_node*& node)
 	{
 		node = 0;
 		return true;
@@ -510,7 +510,7 @@ public:
 	virtual bool matchesWithInfo(ElementDOFMap *otherMap, ElementDOFMapMatchCache& cache) = 0;
 
 	/** override to set flag for each local node in use */
-	virtual void setLocalNodeInUse(int numberOfLocalNodes, int *localNodeInUse)
+	virtual void setLocalNodeInUse(int /*numberOfLocalNodes*/, int */*localNodeInUse*/)
 	{
 	}
 };
