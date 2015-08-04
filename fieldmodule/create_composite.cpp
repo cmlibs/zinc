996466729e0db20e4707cb254851487571b280c8
Richard Christie <r.christie@auckland.ac.nz>
2015-08-04 05:39:14 +0000

    Add tests for multiple component fields and improve single component field tests. https://tracker.physiomeproject.org/show_bug.cgi?id=3781

--- fieldmodule/create_composite.cpp
+++ fieldmodule/create_composite.cpp
@@ -21,113 +21,192 @@
 #include <zinc/fieldcomposite.hpp>
 #include <zinc/fieldconstant.hpp>
 
-TEST(cmzn_fieldmodule_create_field_component, valid_args)
+TEST(cmzn_field_component, single_valid_args)
 {
 	ZincTestSetup zinc;
 
-	const double values[] = { 2.0, 4.0, 6.0 };
+	const double sourceValues[] = { 2.0, 4.0, 6.0 };
+	cmzn_field_id f1 = cmzn_fieldmodule_create_field_constant(zinc.fm, 3, sourceValues);
+	EXPECT_NE((cmzn_field_id)0, f1);
+
 	const int component_index = 2;
+	cmzn_field_id f2 = cmzn_fieldmodule_create_field_component(zinc.fm, f1, component_index);
+	EXPECT_NE((cmzn_field_id)0, f2);
+	EXPECT_EQ(1, cmzn_field_get_number_of_components(f2));
+
+	cmzn_field_component_id fc = cmzn_field_cast_component(f2);
+	EXPECT_NE((cmzn_field_component_id)0, fc);
+
+	cmzn_fieldcache_id cache = cmzn_fieldmodule_create_fieldcache(zinc.fm);
+	double value = 0.0;
+	EXPECT_EQ(CMZN_OK, cmzn_field_evaluate_real(f2, cache, 1, &value));
+	EXPECT_EQ(sourceValues[component_index - 1], value);
+	 
+	int temp_component_index = cmzn_field_component_get_component_index(fc);
+	EXPECT_EQ(component_index, temp_component_index);
+	const int new_component_index = 3;
+	EXPECT_EQ(CMZN_OK, cmzn_field_component_set_component_index(fc, new_component_index));
+	temp_component_index = cmzn_field_component_get_component_index(fc);
+	EXPECT_EQ(new_component_index, temp_component_index);
+	EXPECT_EQ(CMZN_OK, cmzn_field_evaluate_real(f2, cache, 1, &value));
+	EXPECT_EQ(sourceValues[new_component_index - 1], value);
+
+	cmzn_fieldcache_destroy(&cache);
+	cmzn_field_component_destroy(&fc);
+	cmzn_field_destroy(&f1);
+	cmzn_field_destroy(&f2);
+}
+
+TEST(cmzn_field_component, single_invalid_args)
+{
+	ZincTestSetup zinc;
+	const double values[] = { 2.0, 4.0, 6.0 };
 	cmzn_field_id f1 = cmzn_fieldmodule_create_field_constant(zinc.fm, 3, values);
 	EXPECT_NE((cmzn_field_id)0, f1);
 
 	cmzn_field_component_id notComponent = cmzn_field_cast_component(f1);
 	EXPECT_EQ((cmzn_field_component_id)0, notComponent);
 
-	cmzn_field_id f2 = cmzn_fieldmodule_create_field_component(zinc.fm, f1, component_index);
-	EXPECT_NE((cmzn_field_id)0, f2);
-
-	cmzn_field_component_id isComponent = cmzn_field_cast_component(f2);
-	EXPECT_NE((cmzn_field_component_id)0, isComponent);
-	cmzn_field_component_destroy(&isComponent);
-
-	cmzn_fieldcache_id cache = cmzn_fieldmodule_create_fieldcache(zinc.fm);
-	double value = 0.0;
-	EXPECT_EQ(CMZN_OK, cmzn_field_evaluate_real(f2, cache, 1, &value));
-	EXPECT_EQ(values[component_index - 1], value);
-	cmzn_fieldcache_destroy(&cache);
-
-	cmzn_field_destroy(&f1);
-	cmzn_field_destroy(&f2);
-}
-
-TEST(cmzn_fieldmodule_create_field_component, invalid_args)
-{
-	ZincTestSetup zinc;
-	const double values[] = { 2.0, 4.0, 6.0 };
 	const int component_index = 2;
-	cmzn_field_id f1 = cmzn_fieldmodule_create_field_constant(zinc.fm, 3, values);
-	EXPECT_NE((cmzn_field_id)0, f1);
-
 	EXPECT_EQ((cmzn_field_id)0, cmzn_fieldmodule_create_field_component(0, f1, component_index));
 	EXPECT_EQ((cmzn_field_id)0, cmzn_fieldmodule_create_field_component(zinc.fm, 0, component_index));
 	EXPECT_EQ((cmzn_field_id)0, cmzn_fieldmodule_create_field_component(zinc.fm, f1, 0));
 	EXPECT_EQ((cmzn_field_id)0, cmzn_fieldmodule_create_field_component(zinc.fm, f1, 4));
 
+	cmzn_field_id f2 = cmzn_fieldmodule_create_field_component(zinc.fm, f1, component_index);
+	EXPECT_NE((cmzn_field_id)0, f2);
+	EXPECT_EQ(1, cmzn_field_get_number_of_components(f2));
+
+	cmzn_field_component_id fc = cmzn_field_cast_component(f2);
+	EXPECT_NE((cmzn_field_component_id)0, fc);
+
+	EXPECT_EQ(0, cmzn_field_component_get_component_index((cmzn_field_component_id)0));
+	EXPECT_EQ(ERROR_ARGUMENT, cmzn_field_component_set_component_index(fc, 0));
+	EXPECT_EQ(ERROR_ARGUMENT, cmzn_field_component_set_component_index(fc, 4));
+
+	cmzn_field_component_destroy(&fc);
+	cmzn_field_destroy(&f2);
 	cmzn_field_destroy(&f1);
 }
 
-TEST(zincFieldModule_createComponent, valid_args)
+TEST(zincFieldComponent, single_valid_args)
 {
 	ZincTestSetupCpp zinc;
 
-	const double values[] = { 2.0, 4.0, 6.0 };
-	const int component_index = 2;
-	FieldConstant f1 = zinc.fm.createFieldConstant(3, values);
+	const double sourceValues[] = { 2.0, 4.0, 6.0 };
+	FieldConstant f1 = zinc.fm.createFieldConstant(3, sourceValues);
 	EXPECT_TRUE(f1.isValid());
 
-	FieldComponent notComponent = f1.castComponent();
-	EXPECT_FALSE(notComponent.isValid());
-
+	const int component_index = 2;
 	FieldComponent f2 = zinc.fm.createFieldComponent(f1, component_index);
 	EXPECT_TRUE(f2.isValid());
+	EXPECT_EQ(1, f2.getNumberOfComponents());
 
 	FieldComponent isComponent = f2.castComponent();
 	EXPECT_TRUE(isComponent.isValid());
 
 	Fieldcache cache = zinc.fm.createFieldcache();
 	double value = 0.0;
-	EXPECT_EQ(CMZN_OK, f2.evaluateReal(cache, 1, &value));
-	EXPECT_EQ(values[component_index - 1], value);
-}
-
-TEST(zincFieldComponent_setComponentIndex, valid_args)
-{
-	ZincTestSetupCpp zinc;
-
-	const double values[] = { 2.0, 4.0, 6.0 };
-	int component_index = 2;
-	FieldConstant f1 = zinc.fm.createFieldConstant(3, values);
-	EXPECT_TRUE(f1.isValid());
-
-	FieldComponent f2 = zinc.fm.createFieldComponent(f1, component_index);
-	EXPECT_TRUE(f2.isValid());
-
-	Fieldcache cache = zinc.fm.createFieldcache();
-	double value = 0.0;
-	EXPECT_EQ(CMZN_OK, f2.evaluateReal(cache, 1, &value));
-	EXPECT_EQ(values[component_index - 1], value);
+	EXPECT_EQ(OK, f2.evaluateReal(cache, 1, &value));
+	EXPECT_EQ(sourceValues[component_index - 1], value);
 
 	int temp_component_index = f2.getComponentIndex();
 	EXPECT_EQ(component_index, temp_component_index);
-
-	component_index = 3;
-
-	EXPECT_EQ(CMZN_OK, f2.setComponentIndex(component_index));
-
-	EXPECT_EQ(CMZN_OK, f2.evaluateReal(cache, 1, &value));
-	EXPECT_EQ(values[component_index - 1], value);
+	const int new_component_index = 3;
+	EXPECT_EQ(OK, f2.setComponentIndex(new_component_index));
+	temp_component_index = f2.getComponentIndex();
+	EXPECT_EQ(new_component_index, temp_component_index);
+	EXPECT_EQ(OK, f2.evaluateReal(cache, 1, &value));
+	EXPECT_EQ(sourceValues[new_component_index - 1], value);
 }
 
-TEST(zincFieldModule_createComponent, invalid_args)
+TEST(zincFieldComponent, single_invalid_args)
 {
 	ZincTestSetupCpp zinc;
 
-	const double values[] = { 2.0, 4.0, 6.0 };
+	const double sourceValues[] = { 2.0, 4.0, 6.0 };
 	const int component_index = 2;
-	FieldConstant f1 = zinc.fm.createFieldConstant(3, values);
+	FieldConstant f1 = zinc.fm.createFieldConstant(3, sourceValues);
 	EXPECT_TRUE(f1.isValid());
+
+	FieldComponent notComponent = f1.castComponent();
+	EXPECT_FALSE(notComponent.isValid());
 
 	EXPECT_FALSE(zinc.fm.createFieldComponent(Field(), component_index).isValid());
 	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 0).isValid());
 	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 4).isValid());
+
+	FieldComponent f2 = zinc.fm.createFieldComponent(f1, component_index);
+	EXPECT_TRUE(f2.isValid());
+	EXPECT_EQ(1, f2.getNumberOfComponents());
+
+	EXPECT_EQ(0, notComponent.getComponentIndex());
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setComponentIndex(0));
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setComponentIndex(4));
+}
+
+TEST(zincFieldComponent, multiple_valid_args)
+{
+	ZincTestSetupCpp zinc;
+
+	const double sourceValues[] = { 2.0, 4.0, 6.0 };
+	FieldConstant f1 = zinc.fm.createFieldConstant(3, sourceValues);
+	EXPECT_TRUE(f1.isValid());
+
+	const int sourceComponentIndexes[2] = { 2, 1 };
+	FieldComponent f2 = zinc.fm.createFieldComponent(f1, 2, sourceComponentIndexes);
+	EXPECT_TRUE(f2.isValid());
+	EXPECT_EQ(2, f2.getNumberOfComponents());
+
+	FieldComponent isComponent = f2.castComponent();
+	EXPECT_TRUE(isComponent.isValid());
+
+	Fieldcache cache = zinc.fm.createFieldcache();
+	double outValues[2];
+	EXPECT_EQ(OK, f2.evaluateReal(cache, 2, outValues));
+	for (int i = 0; i < 2; ++i)
+		EXPECT_EQ(sourceValues[sourceComponentIndexes[i] - 1], outValues[i]);
+
+	int temp_component_index = f2.getSourceComponentIndex(2);
+	EXPECT_EQ(sourceComponentIndexes[1], temp_component_index);
+	const int new_component_index = 3;
+	EXPECT_EQ(OK, f2.setSourceComponentIndex(2, new_component_index));
+	temp_component_index = f2.getSourceComponentIndex(2);
+	EXPECT_EQ(new_component_index, temp_component_index);
+	EXPECT_EQ(OK, f2.evaluateReal(cache, 2, outValues));
+	EXPECT_EQ(sourceValues[sourceComponentIndexes[0] - 1], outValues[0]);
+	EXPECT_EQ(sourceValues[new_component_index - 1], outValues[1]);
+}
+
+TEST(zincFieldComponent, multiple_invalid_args)
+{
+	ZincTestSetupCpp zinc;
+
+	const double sourceValues[] = { 2.0, 4.0, 6.0 };
+	FieldConstant f1 = zinc.fm.createFieldConstant(3, sourceValues);
+	EXPECT_TRUE(f1.isValid());
+
+	FieldComponent notComponent = f1.castComponent();
+	EXPECT_FALSE(notComponent.isValid());
+
+	const int sourceComponentIndexes[2] = { 2, 1 };
+	EXPECT_FALSE(zinc.fm.createFieldComponent(Field(), 2, sourceComponentIndexes).isValid());
+	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 0, sourceComponentIndexes).isValid());
+	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 2, 0).isValid());
+	const int badSourceComponentIndexes1[2] = { 2, 0 };
+	const int badSourceComponentIndexes2[2] = { 4, 1 };
+	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 2, badSourceComponentIndexes1).isValid());
+	EXPECT_FALSE(zinc.fm.createFieldComponent(f1, 2, badSourceComponentIndexes2).isValid());
+
+	FieldComponent f2 = zinc.fm.createFieldComponent(f1, 2, sourceComponentIndexes);
+	EXPECT_TRUE(f2.isValid());
+	EXPECT_EQ(2, f2.getNumberOfComponents());
+	
+	EXPECT_EQ(0, notComponent.getSourceComponentIndex(1));
+	EXPECT_EQ(0, f2.getSourceComponentIndex(0));
+	EXPECT_EQ(0, f2.getSourceComponentIndex(3));
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setSourceComponentIndex(0, 1));
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setSourceComponentIndex(3, 3));
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setSourceComponentIndex(1, 0));
+	EXPECT_EQ(ERROR_ARGUMENT, f2.setSourceComponentIndex(2, 4));
 }
