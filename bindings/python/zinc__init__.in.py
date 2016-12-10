2bd70329b05ea1683b87fa117314ba954ccda0d2
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-12-10 22:42:43 +1300

    Remove version from binary.  Generate revision string at build time.

--- bindings/python/zinc__init__.in.py
+++ bindings/python/zinc__init__.in.py
@@ -1,5 +1,6 @@
 # Zinc Python package initialisation file.
-__version__ = "@Zinc_VERSION@@ZINC_DEVELOPER_VERSION@"
+__version__ = "@Zinc_VERSION@"
+__revision__ = "@ZINC_DEVELOPER_VERSION@"
 
 # The zinc.dll/zinc.dylib files are in the path where zinc got installed to;
 # we need to add this to the PATH in order to have the import directive find the dependent dlls.
