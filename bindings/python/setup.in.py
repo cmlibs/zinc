a66946a4d14e1145e15a26d25132ae0351f6c37f
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-03-15 14:51:54 +1300

    Add namespace_packages key to setup call.

--- bindings/python/setup.in.py
+++ bindings/python/setup.in.py
@@ -31,6 +31,7 @@ setup(
 	author='H. Sorby',
 	author_email='h.sorby@auckland.ac.nz',
 	packages=['opencmiss', 'opencmiss.zinc'],
+	namespace_packages=['opencmiss'],
 	package_data={'opencmiss.zinc': [@SETUP_PY_PACKAGE_FILES_STR@]},
 	platforms=['any'],
 	url='http://pypi.python.org/pypi/PyZinc/',
